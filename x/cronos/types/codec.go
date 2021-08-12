f778547f15102dc3675ae0e72987b6db229dc92f
yihuang <huang@crypto.com>
2021-08-12 13:46:55 +0800

    Update x/cronos/types/codec.go

--- x/cronos/types/codec.go
+++ x/cronos/types/codec.go
@@ -19,6 +19,5 @@ func RegisterInterfaces(registry cdctypes.InterfaceRegistry) {
 }
 
 var (
-	// amino     = codec.NewLegacyAmino()
 	ModuleCdc = codec.NewProtoCodec(cdctypes.NewInterfaceRegistry())
 )
