0ad0841929de5be3821107a9c413dce113f7b6eb
Thomas Nguy <thomas.nguy@crypto.com>
2021-09-11 00:32:59 +0900

    update CroBridge.sol

--- contracts/src/CroBridge.sol
+++ contracts/src/CroBridge.sol
@@ -1,14 +1,12 @@
 pragma solidity ^0.6.11;
 
-import "ds-token/token.sol";
-
 contract CroBridge {
 
     event __CronosSendToIbc(string recipient, uint256 amount);
 
     // Pay the contract a certain CRO amount and trigger a CRO transfer
     // from the contract to recipient through IBC
-    function send_cro_to_crypto_org(string recipient) public payable {
+    function send_cro_to_crypto_org(string memory recipient) public payable {
         emit __CronosSendToIbc(recipient, msg.value);
     }
 }
