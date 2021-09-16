dc34c0f0fee42346636d97dbaf8833ea4d70ab2b
HuangYi <huang@crypto.com>
2021-09-16 15:46:11 +0800

    Problem: no local gravity bridge setup instructions
    Closes: #52
    
    Solution:
    - document the steps of gravity bridge setup

--- integration_tests/cosmoscli.py
+++ integration_tests/cosmoscli.py
@@ -935,6 +935,11 @@ class CosmosCLI:
             )
         )
 
+    def query_latest_signer_set_tx(self):
+        return json.loads(
+            self.raw("query", "gravity", "latest-signer-set-tx", home=self.data_dir)
+        )
+
     def send_to_ethereum(self, from_, receiver, coins, fee):
         return json.loads(
             self.raw(
