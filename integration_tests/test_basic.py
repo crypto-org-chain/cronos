8042e89b9de5c976cc7123cf19781344874cc4ad
HuangYi <huang@crypto.com>
2021-09-22 12:10:58 +0800

    Problem: tx receipt don't contain events
    Solution:
    - convert tx hash when query logs
    - transaction index in logs start from 0

--- integration_tests/test_basic.py
+++ integration_tests/test_basic.py
@@ -1,12 +1,51 @@
 from pathlib import Path
 
-from .utils import ADDRS, deploy_contract
+from eth_utils import abi
+from hexbytes import HexBytes
+
+from .utils import ADDRS, KEYS, deploy_contract, send_transaction
 
 
 def test_basic(cluster):
     w3 = cluster.w3
     assert w3.eth.chain_id == 777
     assert w3.eth.get_balance(ADDRS["community"]) == 10000000000000000000000
+
+
+def test_events(cluster, suspend_capture):
+    w3 = cluster.w3
+    erc20 = deploy_contract(
+        w3,
+        Path(__file__).parent
+        / "contracts/artifacts/contracts/TestERC20A.sol/TestERC20A.json",
+        key=KEYS["validator"],
+    )
+    tx = erc20.functions.transfer(ADDRS["community"], 10).buildTransaction(
+        {"from": ADDRS["validator"]}
+    )
+    txreceipt = send_transaction(w3, tx, KEYS["validator"])
+    assert len(txreceipt.logs) == 1
+    expect_log = {
+        "address": erc20.address,
+        "topics": [
+            HexBytes(
+                abi.event_signature_to_log_topic("Transfer(address,address,uint256)")
+            ),
+            HexBytes(b"\x00" * 12 + HexBytes(ADDRS["validator"])),
+            HexBytes(b"\x00" * 12 + HexBytes(ADDRS["community"])),
+        ],
+        "data": "0x000000000000000000000000000000000000000000000000000000000000000a",
+        "transactionIndex": 0,
+        "logIndex": 0,
+        "removed": False,
+    }
+    assert expect_log.items() <= txreceipt.logs[0].items()
+
+    # check block bloom
+    assert (
+        w3.eth.get_block(txreceipt.blockNumber).logsBloom
+        == "0x00000800000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000008000002000000000000000000000000000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000400000000000000000000000000000000000000000000000000000000000000000040000000000000000000000000000002000000000000002000000000000000000000000000000000000000000001000000008000000000000000000041000000000000000000000000000000000000000000000"
+    )
 
 
 def test_native_call(cronos):
