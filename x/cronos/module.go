0366e5958ca5f0f8d83b4fc26dc1c581abb0be62
yihuang <huang@crypto.com>
2021-09-27 15:31:28 +0800

    Problem: cronos query api not registered in grpc gateway (#107)
    Closes: #106
    
    Solution:
    - register them

--- x/cronos/module.go
+++ x/cronos/module.go
@@ -1,6 +1,7 @@
 package cronos
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 
@@ -81,6 +82,10 @@ func (AppModuleBasic) RegisterRESTRoutes(clientCtx client.Context, rtr *mux.Rout
 // RegisterGRPCGatewayRoutes registers the gRPC Gateway routes for the module.
 func (AppModuleBasic) RegisterGRPCGatewayRoutes(clientCtx client.Context, mux *runtime.ServeMux) {
 	// this line is used by starport scaffolding # 2
+	if err := types.RegisterQueryHandlerClient(context.Background(), mux, types.NewQueryClient(clientCtx)); err != nil {
+		panic(err)
+	}
+
 }
 
 // GetTxCmd returns the capability module's root tx command.
