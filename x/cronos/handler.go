c86ba0c297dc8f2764cae936797b97e6f570e577
HuangYi <huang@crypto.com>
2021-09-30 18:56:57 +0800

    fix tests

--- x/cronos/handler.go
+++ x/cronos/handler.go
@@ -27,6 +27,9 @@ func NewHandler(k keeper.Keeper) sdk.Handler {
 		case *types.MsgTransferTokens:
 			res, err := msgServer.TransferTokens(sdk.WrapSDKContext(ctx), msg)
 			return sdk.WrapServiceResult(ctx, res, err)
+		case *types.MsgUpdateTokenMapping:
+			res, err := msgServer.UpdateTokenMapping(sdk.WrapSDKContext(ctx), msg)
+			return sdk.WrapServiceResult(ctx, res, err)
 		default:
 			errMsg := fmt.Sprintf("unrecognized %s message type: %T", types.ModuleName, msg)
 			return nil, sdkerrors.Wrap(sdkerrors.ErrUnknownRequest, errMsg)
