8d0b8013ba9f17d076ce5a07642583801f478cda
Calvin Lau <calvin@mona.co>
2021-10-07 04:06:31 +0800

    Fix lint issues

--- cmd/cronosd/config/hdpath.go
+++ cmd/cronosd/config/hdpath.go
@@ -5,7 +5,7 @@ package config
 import ethermint "github.com/tharsis/ethermint/types"
 
 var (
-    BIP44CoinType uint32 = ethermint.Bip44CoinType
+	BIP44CoinType uint32 = ethermint.Bip44CoinType
 
-    BIP44HDPath = ethermint.BIP44HDPath
+	BIP44HDPath = ethermint.BIP44HDPath
 )
