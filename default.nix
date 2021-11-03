cb3e3fbb319dcbfaa5a5d99f78a2efaf666d443e
Damon Chen <damon.chen@crypto.com>
2021-11-03 12:01:42 +0800

    Problem: Confusing ver number default.nix (#199)
    0.5.5 -> dev

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { system ? builtins.currentSystem, pkgs ? import ./nix { inherit system; } }:
 pkgs.buildGoApplication rec {
   pname = "cronosd";
-  version = "0.5.5";
+  version = "dev";
   src = (pkgs.nix-gitignore.gitignoreSourcePure [
     "/*" # ignore all, then add whitelists
     "!/x/"
