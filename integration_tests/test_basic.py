9b918ca1d3494f58405aeee4b4b0b438d66445c9
Damon Chen <damon.chen@crypto.com>
2021-11-04 17:00:43 +0800

    Add test_statesync

--- integration_tests/test_basic.py
+++ integration_tests/test_basic.py
@@ -5,8 +5,7 @@ from eth_bloom import BloomFilter
 from eth_utils import abi, big_endian_to_int
 from hexbytes import HexBytes
 
-from .utils import ADDRS, KEYS, deploy_contract, send_transaction
-
+from .utils import ADDRS, KEYS, deploy_contract, send_transaction, wait_for_block
 
 def test_basic(cluster):
     w3 = cluster.w3
@@ -101,3 +100,55 @@ def test_native_call(cronos):
     )
     receipt = w3.eth.wait_for_transaction_receipt(txhash)
     assert receipt.status == 0, "should fail"
+
+def test_statesync(cronos):
+    ## cronos fixture
+    # Load cronos-devnet.yaml
+    # Spawn pystarport with the yaml, port 26650 (multiple nodes will be created based on `validators`)
+    # Return a Cronos object (Defined in network.py)
+
+    ## do some transactions
+    from_addr = "crc1q04jewhxw4xxu3vlg3rc85240h9q7ns6hglz0g"
+    to_addr = "crc16z0herz998946wr659lr84c8c556da55dc34hh"
+    coins = "10basetcro"
+    node = cronos.node_rpc(0)
+    txhash_1 = cronos.cosmos_cli(0).transfer(from_addr, to_addr, coins)["txhash"]
+    # discovery_time is set to 5 seconds, add extra seconds for processing
+    wait_for_block(cronos.cosmos_cli(0), 15)
+    # Check the transaction is added
+    assert cronos.cosmos_cli(0).query_tx("hash", txhash_1)["txhash"] == txhash_1
+
+    ## add a new state sync node, sync
+    # We can not use the cronos fixture to do statesync, since they are full nodes.
+    # We can only create a new node with statesync config
+    data = Path(cronos.base_dir).parent # Same data dir as cronos fixture
+    chain_id = cronos.config['chain_id'] # Same chain_id as cronos fixture
+    cmd = "cronosd"
+    # create a clustercli object from ClusterCLI class
+    clustercli = cluster.ClusterCLI(data, cmd=cmd, chain_id=chain_id)
+    # create a new node with statesync enabled
+    i = clustercli.create_node(moniker="statesync", statesync=True)
+    clustercli.supervisor.startProcess(f"{clustercli.chain_id}-node{i}")
+    # discovery_time is set to 5 seconds, add extra seconds for processing
+    wait_for_block(clustercli.cosmos_cli(i), 20)
+
+    ## check query chain state works
+    assert clustercli.status(i)["SyncInfo"]["catching_up"] == False
+
+    ## check query old transaction don't work
+    with pytest.raises(AssertionError):
+        clustercli.cosmos_cli(i).query_tx("hash", txhash_1)
+
+    ## execute new transaction
+    # output = clustercli.cosmos_cli(0).transfer(from_addr, to_addr, coins) # this would have problem!
+    txhash_2 = cronos.cosmos_cli(0).transfer(from_addr, to_addr, coins)["txhash"]
+    # discovery_time is set to 5 seconds, add extra seconds for processing
+    wait_for_block(clustercli.cosmos_cli(i), 30)
+
+    ## check query chain state works
+    assert clustercli.status(i)["SyncInfo"]["catching_up"] == False
+
+    ## check query new transaction works
+    assert clustercli.cosmos_cli(i).query_tx("hash", txhash_2)["txhash"] == txhash_2
+
+    print("succesfully syncing")
