813affa09d7b906080f8f0fd17bdfd46a041cd49
Damon Chen <damon.chen@crypto.com>
2021-11-22 18:21:36 +0800

    Use the nix env's solc

--- contracts/shell.nix
+++ contracts/shell.nix
@@ -1,6 +1,7 @@
 { system ? builtins.currentSystem, pkgs ? import ../nix { inherit system; } }:
 pkgs.mkShell {
   buildInputs = [
+    pkgs.which
     pkgs.jq
     pkgs.dapp
     pkgs.solc-versions.solc_0_6_8
