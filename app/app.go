34dd05beabb1faec3cc3a4c7ef98f67e82e81a8b
HuangYi <huang@crypto.com>
2021-12-06 11:45:30 +0800

    add feemarket module

--- app/app.go
+++ app/app.go
@@ -101,6 +101,9 @@ import (
 	evmrest "github.com/tharsis/ethermint/x/evm/client/rest"
 	evmkeeper "github.com/tharsis/ethermint/x/evm/keeper"
 	evmtypes "github.com/tharsis/ethermint/x/evm/types"
+	"github.com/tharsis/ethermint/x/feemarket"
+	feemarketkeeper "github.com/tharsis/ethermint/x/feemarket/keeper"
+	feemarkettypes "github.com/tharsis/ethermint/x/feemarket/types"
 
 	"github.com/peggyjv/gravity-bridge/module/x/gravity"
 	gravitykeeper "github.com/peggyjv/gravity-bridge/module/x/gravity/keeper"
@@ -171,6 +174,7 @@ var (
 		transfer.AppModuleBasic{},
 		vesting.AppModuleBasic{},
 		evm.AppModuleBasic{},
+		feemarket.AppModuleBasic{},
 		gravity.AppModuleBasic{},
 		// this line is used by starport scaffolding # stargate/app/moduleBasic
 		cronos.AppModuleBasic{},
@@ -247,7 +251,8 @@ type App struct {
 	ScopedTransferKeeper capabilitykeeper.ScopedKeeper
 
 	// Ethermint keepers
-	EvmKeeper *evmkeeper.Keeper
+	EvmKeeper       *evmkeeper.Keeper
+	FeeMarketKeeper feemarketkeeper.Keeper
 
 	// Gravity module
 	GravityKeeper gravitykeeper.Keeper
@@ -297,7 +302,7 @@ func New(
 			// ibc keys
 			ibchost.StoreKey, ibctransfertypes.StoreKey,
 			// ethermint keys
-			evmtypes.StoreKey,
+			evmtypes.StoreKey, feemarkettypes.StoreKey,
 			gravitytypes.StoreKey,
 			// this line is used by starport scaffolding # stargate/app/storeKey
 			cronostypes.StoreKey,
@@ -312,7 +317,7 @@ func New(
 			// ibc keys
 			ibchost.StoreKey, ibctransfertypes.StoreKey,
 			// ethermint keys
-			evmtypes.StoreKey,
+			evmtypes.StoreKey, feemarkettypes.StoreKey,
 			// this line is used by starport scaffolding # stargate/app/storeKey
 			cronostypes.StoreKey,
 		)
@@ -401,10 +406,16 @@ func New(
 
 	// Create Ethermint keepers
 	tracer := cast.ToString(appOpts.Get(srvflags.EVMTracer))
+
+	// Create Ethermint keepers
+	app.FeeMarketKeeper = feemarketkeeper.NewKeeper(
+		appCodec, keys[feemarkettypes.StoreKey], app.GetSubspace(feemarkettypes.ModuleName),
+	)
+
 	app.EvmKeeper = evmkeeper.NewKeeper(
 		appCodec, keys[evmtypes.StoreKey], tkeys[evmtypes.TransientKey], app.GetSubspace(evmtypes.ModuleName),
 		app.AccountKeeper, app.BankKeeper, stakingKeeper,
-		&ConstantFeeMarketKeeper{},
+		&app.FeeMarketKeeper,
 		tracer,
 	)
 
@@ -526,6 +537,7 @@ func New(
 
 			transferModule,
 			evm.NewAppModule(app.EvmKeeper, app.AccountKeeper),
+			feemarket.NewAppModule(app.FeeMarketKeeper),
 			gravity.NewAppModule(app.GravityKeeper, app.BankKeeper),
 			// this line is used by starport scaffolding # stargate/app/appModule
 			cronosModule,
@@ -555,6 +567,7 @@ func New(
 
 			transferModule,
 			evm.NewAppModule(app.EvmKeeper, app.AccountKeeper),
+			feemarket.NewAppModule(app.FeeMarketKeeper),
 			// this line is used by starport scaffolding # stargate/app/appModule
 			cronosModule,
 		)
@@ -576,7 +589,7 @@ func New(
 
 		app.mm.SetOrderEndBlockers(
 			crisistypes.ModuleName, govtypes.ModuleName, stakingtypes.ModuleName,
-			evmtypes.ModuleName,
+			evmtypes.ModuleName, feemarkettypes.ModuleName,
 			gravitytypes.ModuleName,
 		)
 	} else {
@@ -590,7 +603,7 @@ func New(
 
 		app.mm.SetOrderEndBlockers(
 			crisistypes.ModuleName, govtypes.ModuleName, stakingtypes.ModuleName,
-			evmtypes.ModuleName,
+			evmtypes.ModuleName, feemarkettypes.ModuleName,
 		)
 	}
 
@@ -617,6 +630,7 @@ func New(
 			authz.ModuleName,
 			feegrant.ModuleName,
 			evmtypes.ModuleName,
+			feemarkettypes.ModuleName,
 			gravitytypes.ModuleName,
 			// this line is used by starport scaffolding # stargate/app/initGenesis
 			cronostypes.ModuleName,
@@ -639,6 +653,7 @@ func New(
 			authz.ModuleName,
 			feegrant.ModuleName,
 			evmtypes.ModuleName,
+			feemarkettypes.ModuleName,
 			// this line is used by starport scaffolding # stargate/app/initGenesis
 			cronostypes.ModuleName,
 		)
@@ -671,6 +686,8 @@ func New(
 		authzmodule.NewAppModule(appCodec, app.AuthzKeeper, app.AccountKeeper, app.BankKeeper, app.interfaceRegistry),
 		ibc.NewAppModule(app.IBCKeeper),
 		transferModule,
+		evm.NewAppModule(app.EvmKeeper, app.AccountKeeper),
+		feemarket.NewAppModule(app.FeeMarketKeeper),
 	)
 
 	app.sm.RegisterStoreDecoders()
@@ -688,7 +705,7 @@ func New(
 	app.SetAnteHandler(
 		evmante.NewAnteHandler(
 			app.AccountKeeper, app.BankKeeper, app.EvmKeeper, app.FeeGrantKeeper, app.IBCKeeper.ChannelKeeper,
-			&ConstantFeeMarketKeeper{},
+			&app.FeeMarketKeeper,
 			encodingConfig.TxConfig.SignModeHandler(),
 		),
 	)
@@ -869,6 +886,7 @@ func initParamsKeeper(appCodec codec.BinaryCodec, legacyAmino *codec.LegacyAmino
 	paramsKeeper.Subspace(ibctransfertypes.ModuleName)
 	paramsKeeper.Subspace(ibchost.ModuleName)
 	paramsKeeper.Subspace(evmtypes.ModuleName)
+	paramsKeeper.Subspace(feemarkettypes.ModuleName)
 	if experimental {
 		paramsKeeper.Subspace(gravitytypes.ModuleName)
 	}
