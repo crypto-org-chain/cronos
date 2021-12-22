10e0fe324df72b46dccf1aafbd675d88c5f679b3
Damon Chen <damon.chen@crypto.com>
2021-12-22 11:57:33 +0800

    Feat: Add `CONTRACTS` dict for contract paths
    Closes: #271
    
    Solution:
    - Add `CONTRACTS` dict for contract paths.
    - TEST_CONTRACT_PATH: the common path.

--- integration_tests/test_exported_genesis.py
+++ integration_tests/test_exported_genesis.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import pytest
 
 from .network import setup_custom_cronos
-from .utils import ADDRS
+from .utils import ADDRS, CONTRACTS
 
 
 @pytest.fixture(scope="module")
@@ -18,12 +18,7 @@ def custom_cronos(tmp_path_factory):
 def test_exported_contract(custom_cronos):
     "demonstrate that contract state can be deployed in genesis"
     w3 = custom_cronos.w3
-    abi = json.load(
-        (
-            Path(__file__).parent
-            / "contracts/artifacts/contracts/TestERC20Utility.sol/TestERC20Utility.json"
-        ).open()
-    )["abi"]
+    abi = json.load((CONTRACTS["TestERC20Utility"]).open())["abi"]
     erc20 = w3.eth.contract(
         address="0x68542BD12B41F5D51D6282Ec7D91D7d0D78E4503", abi=abi
     )
