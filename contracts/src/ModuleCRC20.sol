ad54314501a02f588dca19da31f2b4d6015b129c
yihuang <huang@crypto.com>
2022-01-26 10:29:19 +0800

    Problem: comment in ModuleCRC20.sol is out of date (#319)
    update the comment on module address.

--- contracts/src/ModuleCRC20.sol
+++ contracts/src/ModuleCRC20.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.6.8;
 import "ds-token/token.sol";
 
 contract ModuleCRC20 is DSToken  {
-    // sha256('cronos')[:20]
+    // sha256('cronos-evm')[:20]
     address constant module_address = 0x89A7EF2F08B1c018D5Cc88836249b84Dd5392905;
     string denom;
 
