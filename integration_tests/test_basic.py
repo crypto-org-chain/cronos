6928585eb6b5278a4477f467c35d86a0828904c8
yihuang <huang@crypto.com>
2022-03-02 19:29:49 +0800

    Problem: no integration test for websocket rpc endpoint (#370)
    * Problem: no integration test for websocket rpc endpoint
    
    Closes: #364
    Solution:
    - add cronos-ws provider to run exiting test cases on websocket endpoint
    - add websocket subscription test cases
    - update ethermint to fix websocket bugs
    
    * fix python linter
    
    * changelog
    
    * fix python linter

--- integration_tests/test_basic.py
+++ integration_tests/test_basic.py
@@ -21,7 +21,6 @@ from .utils import (
 def test_basic(cluster):
     w3 = cluster.w3
     assert w3.eth.chain_id == 777
-    assert w3.eth.get_balance(ADDRS["community"]) == 10000000000000000000000
 
 
 def test_events(cluster, suspend_capture):
