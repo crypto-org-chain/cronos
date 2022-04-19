9858814d7639781112cc45ecbb0f524cd89be102
HuangYi <huang@crypto.com>
2022-04-19 14:41:46 +0800

    update poetry2nix overrides

--- nix/testenv.nix
+++ nix/testenv.nix
@@ -7,5 +7,34 @@ pkgs.poetry2nix.mkPoetryEnv {
         substituteInPlace setup.py --replace \'setuptools-markdown\' ""
       '';
     };
+
+    # https://github.com/nix-community/poetry2nix/issues/218#issuecomment-981615612
+    tomli = super.tomli.overridePythonAttrs (
+      old: {
+        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ self.flit-core ];
+      }
+    );
+
+    typing-extensions = super.typing-extensions.overridePythonAttrs (
+      old: {
+        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ self.flit-core ];
+      }
+    );
+
+    platformdirs = pkgs.python3Packages.platformdirs;
+
+    black = super.black.overridePythonAttrs (
+      old: {
+        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ self.flit-core ];
+        buildInputs = (old.buildInputs or [ ]) ++ [ self.platformdirs ];
+      }
+    );
+
+    pyparsing = super.pyparsing.overridePythonAttrs (
+      old: {
+        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ self.flit-core ];
+      }
+    );
+
   });
 }
