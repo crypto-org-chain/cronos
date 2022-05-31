13021a34bb2692613e578cd4600f351f13239b7c
Thomas Nguy <thomas.nguy@crypto.com>
2022-06-01 01:01:43 +0900

    fix contract

--- integration_tests/contracts/contracts/CronosGravityCancellation.sol
+++ integration_tests/contracts/contracts/CronosGravityCancellation.sol
@@ -5,7 +5,7 @@ contract CronosGravityCancellation {
     event __CronosCancelSendToEthereum(uint256 id);
 
     // Cancel a send to ethereum transaction considering if it hasnt been batched yet.
-    function cancelTransaction(uint256 memory id) public {
+    function cancelTransaction(uint256 id) public {
         emit __CronosCancelSendToEthereum(id);
     }
 }
