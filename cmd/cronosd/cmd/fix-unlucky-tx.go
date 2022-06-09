32645d3657a215997ace21de337ee6efee4891de
Leo Pang <34628052+allthatjazzleo@users.noreply.github.com>
2022-06-09 14:33:17 +0800

    Problem: 0.7.x fix-unlucky-tx command does not include concurrency and null indexer (#543)
    * dd concurrency in fix-unlucky-tx command and supports null tx indexer in
    0.7.x
    
    * fix ci lint
    
    * fix ci import lint
    
    * fix cmd arg and don't print the txIndex in scanning mode
    
    * fix scanning mode logic flow
    
    * fix integration test and fix-unlucky-tx usage
    
    * ignore non-eth tx when patching unlucky tx
    
    * Update cmd/cronosd/cmd/fix-unlucky-tx.go
    
    Co-authored-by: Delweng <delweng@gmail.com>
    Co-authored-by: yihuang <huang@crypto.com>

--- cmd/cronosd/cmd/fix-unlucky-tx.go
+++ cmd/cronosd/cmd/fix-unlucky-tx.go
@@ -3,12 +3,14 @@ package cmd
 import (
 	"bufio"
 	"bytes"
+	"context"
 	"errors"
 	"fmt"
-	"io"
 	"os"
+	"runtime"
 	"strconv"
 	"strings"
+	"sync"
 
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
@@ -21,23 +23,24 @@ import (
 	"github.com/tendermint/tendermint/state/indexer/sink/psql"
 	"github.com/tendermint/tendermint/state/txindex"
 	"github.com/tendermint/tendermint/state/txindex/kv"
+	"github.com/tendermint/tendermint/state/txindex/null"
 	tmstore "github.com/tendermint/tendermint/store"
 	evmtypes "github.com/tharsis/ethermint/x/evm/types"
 )
 
 const (
-	FlagMinBlockHeight = "min-block-height"
-
+	FlagMinBlockHeight       = "min-block-height"
 	ExceedBlockGasLimitError = "out of gas in location: block gas meter; gasWanted:"
+	FlagPrintBlockNumbers    = "print-block-numbers"
 )
 
 // FixUnluckyTxCmd update the tx execution result of false-failed tx in tendermint db
 func FixUnluckyTxCmd() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "fix-unlucky-tx [blocks-file]",
+		Use:   "fix-unlucky-tx",
 		Short: "Fix tx execution result of false-failed tx after v0.7.0 upgrade, \"-\" means stdin.",
 		Long:  "Fix tx execution result of false-failed tx after v0.7.0 upgrade.\nWARNING: don't use this command to patch blocks generated before v0.7.0 upgrade",
-		Args:  cobra.ExactArgs(1),
+		Args:  cobra.ExactArgs(0),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			ctx := server.GetServerContextFromCmd(cmd)
 			clientCtx := client.GetClientContextFromCmd(cmd)
@@ -51,17 +54,13 @@ func FixUnluckyTxCmd() *cobra.Command {
 			if err != nil {
 				return err
 			}
-
-			var blocksFile io.Reader
-			if args[0] == "-" {
-				blocksFile = os.Stdin
-			} else {
-				fp, err := os.Open(args[0])
-				if err != nil {
-					return err
-				}
-				defer fp.Close()
-				blocksFile = fp
+			dryRun, err := cmd.Flags().GetBool(flags.FlagDryRun)
+			if err != nil {
+				return err
+			}
+			printBlockNumbers, err := cmd.Flags().GetBool(FlagPrintBlockNumbers)
+			if err != nil {
+				return err
 			}
 
 			tmDB, err := openTMDB(ctx.Config, chainID)
@@ -69,36 +68,37 @@ func FixUnluckyTxCmd() *cobra.Command {
 				return err
 			}
 
-			scanner := bufio.NewScanner(blocksFile)
-			for scanner.Scan() {
-				blockNumber, err := strconv.ParseInt(scanner.Text(), 10, 64)
-				if err != nil {
-					return err
-				}
+			// patch a single block
+			processBlock := func(height int64) error {
 
-				if blockNumber < int64(minBlockHeight) {
-					return fmt.Errorf("block number is generated before v0.7.0 upgrade: %d", blockNumber)
+				if height < int64(minBlockHeight) {
+					return fmt.Errorf("block number is generated before v0.7.0 upgrade: %d", height)
 				}
 
 				// load results
-				blockResults, err := tmDB.stateStore.LoadABCIResponses(blockNumber)
+				blockResults, err := tmDB.stateStore.LoadABCIResponses(height)
 				if err != nil {
 					return err
 				}
 
-				// find unlucky tx
+				// find and patch unlucky tx
 				var txIndex int64
 				for i, txResult := range blockResults.DeliverTxs {
 					if TxExceedsBlockGasLimit(txResult) {
 						if len(txResult.Events) > 0 && txResult.Events[len(txResult.Events)-1].Type == evmtypes.TypeMsgEthereumTx {
 							// already patched
-							break
+							return nil
+						}
+
+						if printBlockNumbers {
+							fmt.Println(height)
+							return nil
 						}
 
 						// load raw tx
-						blk := tmDB.blockStore.LoadBlock(blockNumber)
+						blk := tmDB.blockStore.LoadBlock(height)
 						if blk == nil {
-							return fmt.Errorf("block not found: %d", blockNumber)
+							return fmt.Errorf("block not found: %d", height)
 						}
 
 						tx, err := clientCtx.TxConfig.TxDecoder()(blk.Txs[i])
@@ -106,13 +106,15 @@ func FixUnluckyTxCmd() *cobra.Command {
 							return err
 						}
 
+						if _, ok := tx.GetMsgs()[0].(*evmtypes.MsgEthereumTx); !ok {
+							// ignore non-eth tx
+							break
+						}
+
 						txIndex++
 						for msgIndex, msg := range tx.GetMsgs() {
 							ethTxIndex := txIndex + int64(msgIndex)
-							ethTx, ok := msg.(*evmtypes.MsgEthereumTx)
-							if !ok {
-								continue
-							}
+							ethTx := msg.(*evmtypes.MsgEthereumTx)
 							evt := abci.Event{
 								Type: evmtypes.TypeMsgEthereumTx,
 								Attributes: []abci.EventAttribute{
@@ -122,11 +124,16 @@ func FixUnluckyTxCmd() *cobra.Command {
 							}
 							txResult.Events = append(txResult.Events, evt)
 						}
-						if err := tmDB.stateStore.SaveABCIResponses(blockNumber, blockResults); err != nil {
+
+						if dryRun {
+							return clientCtx.PrintProto(txResult)
+						}
+
+						if err := tmDB.stateStore.SaveABCIResponses(height, blockResults); err != nil {
 							return err
 						}
 						if err := tmDB.txIndexer.Index(&abci.TxResult{
-							Height: blockNumber,
+							Height: height,
 							Index:  uint32(i),
 							Tx:     blk.Txs[i],
 							Result: *txResult,
@@ -134,10 +141,14 @@ func FixUnluckyTxCmd() *cobra.Command {
 							return err
 						}
 						for _, msg := range tx.GetMsgs() {
-							fmt.Println("patched", blockNumber, msg.(*evmtypes.MsgEthereumTx).Hash)
+							fmt.Println("patched", height, msg.(*evmtypes.MsgEthereumTx).Hash)
 						}
-						break
+						return nil
 					} else if txResult.Code == 0 {
+						if printBlockNumbers {
+							continue
+						}
+
 						// find the correct tx index
 						for _, evt := range txResult.Events {
 							if evt.Type == evmtypes.TypeMsgEthereumTx {
@@ -153,13 +164,105 @@ func FixUnluckyTxCmd() *cobra.Command {
 						}
 					}
 				}
+				return nil
 			}
 
-			return nil
+			blocksFile, err := cmd.Flags().GetString(FlagBlocksFile)
+			if err != nil {
+				return err
+			}
+			concurrency, err := cmd.Flags().GetInt(FlagConcurrency)
+			if err != nil {
+				return err
+			}
+
+			blockChan := make(chan int64, concurrency)
+			var wg sync.WaitGroup
+			ctCtx, cancel := context.WithCancel(context.Background())
+			defer cancel()
+
+			for i := 0; i < concurrency; i++ {
+				wg.Add(1)
+				go func(wg *sync.WaitGroup) {
+					defer wg.Done()
+					for {
+						select {
+						case <-ctCtx.Done():
+							return
+						case blockNum, ok := <-blockChan:
+							if !ok {
+								return
+							}
+
+							if err := processBlock(blockNum); err != nil {
+								cancel()
+								return
+							}
+						}
+					}
+				}(&wg)
+			}
+
+			findBlock := func() error {
+				if len(blocksFile) > 0 {
+					// read block numbers from file, one number per line
+					file, err := os.Open(blocksFile)
+					if err != nil {
+						return err
+					}
+					defer file.Close()
+					scanner := bufio.NewScanner(file)
+					for scanner.Scan() {
+						blockNumber, err := strconv.ParseInt(scanner.Text(), 10, 64)
+						if err != nil {
+							return err
+						}
+						blockChan <- blockNumber
+					}
+				} else {
+					startHeight, err := cmd.Flags().GetInt(FlagStartBlock)
+					if err != nil {
+						return err
+					}
+					endHeight, err := cmd.Flags().GetInt(FlagEndBlock)
+					if err != nil {
+						return err
+					}
+					if startHeight < 1 {
+						return fmt.Errorf("invalid start-block: %d", startHeight)
+					}
+					if endHeight < startHeight {
+						return fmt.Errorf("invalid end-block %d, smaller than start-block", endHeight)
+					}
+
+					for height := startHeight; height <= endHeight; height++ {
+						blockChan <- int64(height)
+					}
+				}
+				return nil
+			}
+
+			go func() {
+				err := findBlock()
+				if err != nil {
+					fmt.Fprintln(os.Stderr, err)
+				}
+				close(blockChan)
+			}()
+
+			wg.Wait()
+
+			return ctCtx.Err()
 		},
 	}
 	cmd.Flags().String(flags.FlagChainID, "cronosmainnet_25-1", "network chain ID, only useful for psql tx indexer backend")
 	cmd.Flags().Int(FlagMinBlockHeight, 2693800, "The block height v0.7.0 upgrade executed, will reject block heights smaller than this.")
+	cmd.Flags().Bool(flags.FlagDryRun, false, "Print the execution result of the problematic txs without patch the database")
+	cmd.Flags().Bool(FlagPrintBlockNumbers, false, "Print the problematic block number without patch")
+	cmd.Flags().String(FlagBlocksFile, "", "Read block numbers from a file instead of iterating all the blocks")
+	cmd.Flags().Int(FlagStartBlock, 1, "The start of the block range to iterate, inclusive")
+	cmd.Flags().Int(FlagEndBlock, -1, "The end of the block range to iterate, inclusive")
+	cmd.Flags().Int(FlagConcurrency, runtime.NumCPU(), "Define how many workers run in concurrency")
 
 	return cmd
 }
@@ -213,7 +316,7 @@ func newTxIndexer(config *tmcfg.Config, chainID string) (txindex.TxIndexer, erro
 		}
 		return es.TxIndexer(), nil
 	default:
-		return nil, fmt.Errorf("unsupported tx indexer backend %s", config.TxIndex.Indexer)
+		return &null.TxIndex{}, nil
 	}
 }
 
