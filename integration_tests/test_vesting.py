4be97023887cd3cd7069f2ea68e09da7c8c05ec1
mmsqe <mavis@crypto.com>
2022-08-17 12:13:01 +0800

    Problem: create_vesting_account is not tested (#648)
    * add create_vesting_account test
    
    * more var for end_time
    
    * add end_time param

---
+++ integration_tests/test_vesting.py
@@ -0,0 +1,22 @@
+import time
+
+
+def test_create_account(cronos):
+    """
+    test create vesting account tx works:
+    """
+    cli = cronos.cosmos_cli()
+    src = "vesting"
+    addr = cli.create_account(src)["address"]
+    denom = "basetcro"
+    balance = cli.balance(addr, denom)
+    assert balance == 0
+    amount = 10000
+    fee = 4000000000000000
+    amt = f"{amount}{denom}"
+    end_time = int(time.time()) + 3000
+    fees = f"{fee}{denom}"
+    res = cli.create_vesting_account(addr, amt, end_time, from_="validator", fees=fees)
+    assert res["code"] == 0, res["raw_log"]
+    balance = cli.balance(addr, denom)
+    assert balance == amount
