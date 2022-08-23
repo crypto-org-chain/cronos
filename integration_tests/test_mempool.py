e733e4f8780b8a1a34388646a43ae67b287c79e4
mmsqe <mavis@crypto.com>
2022-08-23 09:07:19 +0800

    retry when new block is generated

--- integration_tests/test_mempool.py
+++ integration_tests/test_mempool.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import pytest
 from web3 import Web3
 from web3.exceptions import TransactionNotFound
+
 from .network import setup_custom_cronos
 from .utils import (
     ADDRS,
@@ -25,67 +26,75 @@ def cronos_mempool(tmp_path_factory):
 
 
 def test_mempool(cronos_mempool):
-    w3: Web3 = cronos_mempool.w3
-    filter = w3.eth.filter("pending")
-    assert filter.get_new_entries() == []
+    max_retry = 3
+    for x in range(max_retry):
+        w3: Web3 = cronos_mempool.w3
+        filter = w3.eth.filter("pending")
+        assert filter.get_new_entries() == []
 
-    cli = cronos_mempool.cosmos_cli(0)
-    # test contract
-    wait_for_new_blocks(cli, 1, sleep=0.1)
-    block_num_2 = w3.eth.get_block_number()
-    print(f"block number contract begin at height: {block_num_2}")
-    contract = deploy_contract(w3, CONTRACTS["Greeter"])
-    tx = contract.functions.setGreeting("world").buildTransaction()
-    signed = sign_transaction(w3, tx)
-    txhash = w3.eth.send_raw_transaction(signed.rawTransaction)
-    w3.eth.wait_for_transaction_receipt(txhash)
-    # check tx in mempool
-    new_txs = filter.get_new_entries()
-    assert txhash in new_txs
-
-    greeter_call_result = contract.caller.greet()
-    assert "world" == greeter_call_result
-
-    # check mempool
-    all_pending = w3.eth.get_filter_changes(filter.filter_id)
-    print(f"all pending tx hash after block: {all_pending}")
-    assert len(all_pending) == 0
-
-    to = ADDRS["community"]
-    params = {"gasPrice": w3.eth.gas_price}
-    block_num_0, sended_hash_set = send_txs(w3, cli, to, KEYS.values(), params)
-
-    all_pending = w3.eth.get_filter_changes(filter.filter_id)
-    assert len(all_pending) == 0
-
-    block_num_1 = w3.eth.get_block_number()
-    assert block_num_1 == block_num_0, "new block is generated, fail"
-    print(f"all send tx hash: f{sended_hash_set}")
-
-    # check after max 10 blocks
-    for i in range(10):
-        all_pending = w3.eth.get_filter_changes(filter.filter_id)
-        print(f"all pending tx hash at block {i+block_num_1}: {all_pending}")
-        for hash in all_pending:
-            sended_hash_set.discard(hash)
-        if len(sended_hash_set) == 0:
-            break
-
-        # FIXME remove after new design with subscription
-        with ThreadPoolExecutor(len(sended_hash_set)) as exec:
-            tasks = [
-                exec.submit(w3.eth.get_transaction, hash) for hash in sended_hash_set
-            ]
-            try:
-                for future in as_completed(tasks):
-                    res = future.result()
-                    if res.blockNumber is not None:
-                        print(f"{res.hash.hex()} confirmed at {res.blockNumber}")
-                        sended_hash_set.discard(res.hash)
-            except TransactionNotFound as e:
-                print("get txs fail", e)
-
-        if len(sended_hash_set) == 0:
-            break
+        cli = cronos_mempool.cosmos_cli(0)
+        # test contract
         wait_for_new_blocks(cli, 1, sleep=0.1)
-    assert len(sended_hash_set) == 0
+        block_num_2 = w3.eth.get_block_number()
+        print(f"block number contract begin at height: {block_num_2}")
+        contract = deploy_contract(w3, CONTRACTS["Greeter"])
+        tx = contract.functions.setGreeting("world").buildTransaction()
+        signed = sign_transaction(w3, tx)
+        txhash = w3.eth.send_raw_transaction(signed.rawTransaction)
+        w3.eth.wait_for_transaction_receipt(txhash)
+        # check tx in mempool
+        new_txs = filter.get_new_entries()
+        assert txhash in new_txs
+
+        greeter_call_result = contract.caller.greet()
+        assert "world" == greeter_call_result
+
+        # check mempool
+        all_pending = w3.eth.get_filter_changes(filter.filter_id)
+        print(f"all pending tx hash after block: {all_pending}")
+        assert len(all_pending) == 0
+
+        to = ADDRS["community"]
+        params = {"gasPrice": w3.eth.gas_price}
+        block_num_0, sended_hash_set = send_txs(w3, cli, to, KEYS.values(), params)
+
+        all_pending = w3.eth.get_filter_changes(filter.filter_id)
+        assert len(all_pending) == 0
+
+        block_num_1 = w3.eth.get_block_number()
+        try:
+            assert block_num_1 == block_num_0, "new block is generated"
+            print(f"all send tx hash: f{sended_hash_set}")
+        except AssertionError as e:
+            if x < max_retry - 1:
+                print(f"retry {x} failed:", e)
+                continue
+
+        # check after max 10 blocks
+        for i in range(10):
+            all_pending = w3.eth.get_filter_changes(filter.filter_id)
+            print(f"all pending tx hash at block {i+block_num_1}: {all_pending}")
+            for h in all_pending:
+                sended_hash_set.discard(h)
+            if len(sended_hash_set) == 0:
+                break
+
+            # FIXME remove after new design with subscription
+            with ThreadPoolExecutor(len(sended_hash_set)) as exec:
+                tasks = [
+                    exec.submit(w3.eth.get_transaction, h) for h in sended_hash_set
+                ]
+                try:
+                    for future in as_completed(tasks):
+                        res = future.result()
+                        if res.blockNumber is not None:
+                            print(f"{res.hash.hex()} confirmed at {res.blockNumber}")
+                            sended_hash_set.discard(res.hash)
+                except TransactionNotFound as e:
+                    print("get txs fail", e)
+
+            if len(sended_hash_set) == 0:
+                break
+            wait_for_new_blocks(cli, 1, sleep=0.1)
+        assert len(sended_hash_set) == 0
+        break
