61121eabd66bb3aa6c47bcbd1f4a2525636e0b2b
Thomas Nguy <thomas.nguy@crypto.com>
2022-12-22 14:11:29 +0900

    add unit test

---
+++ x/cronos/keeper/permissions_test.go
@@ -0,0 +1,60 @@
+package keeper_test
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	. "github.com/crypto-org-chain/cronos/x/cronos/keeper"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/evmos/ethermint/crypto/ethsecp256k1"
+)
+
+func (suite *KeeperTestSuite) TestSetAndGetPermissions() {
+	suite.SetupTest()
+	keeper := suite.app.CronosKeeper
+
+	// generate test address
+	priv, err := ethsecp256k1.GenerateKey()
+	suite.Require().NoError(err)
+	address := common.BytesToAddress(priv.PubKey().Address().Bytes())
+	cosmosAddress := sdk.AccAddress(address.Bytes())
+
+	permissions := keeper.GetPermissions(suite.ctx, cosmosAddress)
+	suite.Require().Equal(uint64(0), permissions)
+
+	keeper.SetPermissions(suite.ctx, cosmosAddress, CanChangeTokenMapping)
+	permissions = keeper.GetPermissions(suite.ctx, cosmosAddress)
+	suite.Require().Equal(CanChangeTokenMapping, permissions)
+
+	keeper.SetPermissions(suite.ctx, cosmosAddress, CanTurnBridge)
+	permissions = keeper.GetPermissions(suite.ctx, cosmosAddress)
+	suite.Require().Equal(CanTurnBridge, permissions)
+
+	keeper.SetPermissions(suite.ctx, cosmosAddress, All)
+	permissions = keeper.GetPermissions(suite.ctx, cosmosAddress)
+	suite.Require().Equal(All, permissions)
+}
+
+func (suite *KeeperTestSuite) TestHasPermissions() {
+	suite.SetupTest()
+	keeper := suite.app.CronosKeeper
+
+	// generate test address
+	priv, err := ethsecp256k1.GenerateKey()
+	suite.Require().NoError(err)
+	address := common.BytesToAddress(priv.PubKey().Address().Bytes())
+	cosmosAddress := sdk.AccAddress(address.Bytes())
+
+	suite.Require().Equal(false, keeper.HasPermission(suite.ctx, cosmosAddress, CanChangeTokenMapping))
+	suite.Require().Equal(false, keeper.HasPermission(suite.ctx, cosmosAddress, CanTurnBridge))
+
+	keeper.SetPermissions(suite.ctx, cosmosAddress, CanChangeTokenMapping)
+	suite.Require().Equal(true, keeper.HasPermission(suite.ctx, cosmosAddress, CanChangeTokenMapping))
+	suite.Require().Equal(false, keeper.HasPermission(suite.ctx, cosmosAddress, CanTurnBridge))
+
+	keeper.SetPermissions(suite.ctx, cosmosAddress, CanTurnBridge)
+	suite.Require().Equal(false, keeper.HasPermission(suite.ctx, cosmosAddress, CanChangeTokenMapping))
+	suite.Require().Equal(true, keeper.HasPermission(suite.ctx, cosmosAddress, CanTurnBridge))
+
+	keeper.SetPermissions(suite.ctx, cosmosAddress, All)
+	suite.Require().Equal(true, keeper.HasPermission(suite.ctx, cosmosAddress, CanChangeTokenMapping))
+	suite.Require().Equal(true, keeper.HasPermission(suite.ctx, cosmosAddress, CanTurnBridge))
+}
