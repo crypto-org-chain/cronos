d50bbaccf2f2fa87ea612f81c9c5fdbb8f0a5c91
HuangYi <huang@crypto.com>
2023-01-19 00:31:26 +0800

    rocksdb options

--- cmd/cronosd/cmd/open_db_rocksdb.go
+++ cmd/cronosd/cmd/open_db_rocksdb.go
@@ -14,7 +14,7 @@ func openDB(rootDir string, backendType dbm.BackendType) (dbm.DB, error) {
 	dataDir := filepath.Join(rootDir, "data")
 	if backendType == dbm.RocksDBBackend {
 		// customize rocksdb options
-		db, err := grocksdb.OpenDb(grocksdb.NewDefaultOptions(), filepath.Join(dataDir, "application.db"))
+		db, err := grocksdb.OpenDb(newRocksdbOptions(), filepath.Join(dataDir, "application.db"))
 		if err != nil {
 			return nil, err
 		}
@@ -26,4 +26,30 @@ func openDB(rootDir string, backendType dbm.BackendType) (dbm.DB, error) {
 	} else {
 		return dbm.NewDB("application", backendType, dataDir)
 	}
+}
+
+func newRocksdbOptions() *grocksdb.Options {
+	opts := grocksdb.NewDefaultOptions()
+	opts.SetComparator(CreateTSComparator())
+	opts.SetTargetFileSizeMultiplier(2)
+	opts.SetLevelCompactionDynamicLevelBytes(true)
+
+	// block based table options
+	blkOpts := grocksdb.NewDefaultBlockBasedTableOptions()
+	blkOpts.SetBlockSize(32 * 1024)
+	blkOpts.SetFilterPolicy(grocksdb.NewRibbonHybridFilterPolicy(9.9, 1))
+	blkOpts.SetIndexType(grocksdb.KTwoLevelIndexSearchIndexType)
+	blkOpts.SetPartitionFilters(true)
+	blkOpts.SetDataBlockIndexType(grocksdb.KDataBlockIndexTypeBinarySearchAndHash)
+	opts.SetBlockBasedTableFactory(blkOpts)
+	opts.SetOptimizeFiltersForHits(true)
+
+	// compression options at bottommost level
+	opts.SetBottommostCompression(grocksdb.ZSTDCompression)
+	compressOpts := grocksdb.NewDefaultCompressionOptions()
+	compressOpts.MaxDictBytes = 112640 // 110k
+	compressOpts.Level = 12
+	opts.SetBottommostCompressionOptions(compressOpts, true)
+	opts.SetBottommostCompressionOptionsZstdMaxTrainBytes(compressOpts.MaxDictBytes*100, true)
+	return opts
 }
