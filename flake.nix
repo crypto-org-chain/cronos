490c70225488feaab0ebded024ef42d27ad8c8b8
HuangYi <huang@crypto.com>
2023-01-31 17:14:52 +0800

    revert nixpkgs change

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-22.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
     flake-utils.url = "github:numtide/flake-utils";
     nix-bundle-exe = {
       url = "github:3noch/nix-bundle-exe";
