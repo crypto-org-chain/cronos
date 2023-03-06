bd444354a6bbc1544d25f6d5c85bd401317a2c9f
mmsqe <mavis@crypto.com>
2023-03-06 09:45:47 +0800

    fix lint

--- integration_tests/test_basic.py
+++ integration_tests/test_basic.py
@@ -677,9 +677,10 @@ def test_tx_inclusion(cronos, max_gas_wanted):
 
     test against different max-gas-wanted configuration.
     """
+    cmd = f"cronosd start --home . --trace --evm.max-tx-gas-wanted {max_gas_wanted}"
     modify_command_in_supervisor_config(
         cronos.base_dir / "tasks.ini",
-        lambda _: f"cronosd start --home . --trace --evm.max-tx-gas-wanted {max_gas_wanted}",
+        lambda _: cmd,
     )
     cronos.supervisorctl("update")
     wait_for_port(ports.evmrpc_port(cronos.base_port(0)))
