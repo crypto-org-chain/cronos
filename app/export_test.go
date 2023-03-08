09a101f5c90093ba81a7ac933f48fd7d7733f0b2
mmsqe <mavis@crypto.com>
2023-03-08 10:09:48 +0800

    handle iterator error in export

---
+++ app/export_test.go
@@ -0,0 +1,36 @@
+package app
+
+import (
+	"testing"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/evmos/ethermint/crypto/ethsecp256k1"
+	"github.com/stretchr/testify/require"
+)
+
+func TestExportAppStateAndValidators(t *testing.T) {
+	testCases := []struct {
+		name          string
+		forZeroHeight bool
+	}{
+		{
+			"for zero height",
+			true,
+		},
+		{
+			"for non-zero height",
+			false,
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			privKey, err := ethsecp256k1.GenerateKey()
+			require.NoError(t, err)
+			address := sdk.AccAddress(privKey.PubKey().Address())
+			app := Setup(t, address.String())
+			app.Commit()
+			_, err = app.ExportAppStateAndValidators(tc.forZeroHeight, []string{})
+			require.NoError(t, err, "ExportAppStateAndValidators should not have an error")
+		})
+	}
+}
