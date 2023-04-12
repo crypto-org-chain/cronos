d5c7d484daa238cd6a99945d3a9fc60334df1a01
yihuang <huang@crypto.com>
2023-04-12 20:55:11 +0800

    Problem: v1.0.7 is not released (#971)
    prepare changelog and version number

--- default.nix
+++ default.nix
@@ -9,7 +9,7 @@
 , static ? stdenv.hostPlatform.isStatic
 }:
 let
-  version = "v1.0.6";
+  version = "v1.0.7";
   pname = "cronosd";
   tags = [ "ledger" "netgo" network "rocksdb" "grocksdb_no_link" ];
   ldflags = lib.concatStringsSep "\n" ([
