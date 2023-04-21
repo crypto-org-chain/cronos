9dffd0884555589ee3a7345e77e637c8c32342ab
yihuang <huang@crypto.com>
2023-04-21 14:36:12 +0800

    Problem: NodeLayout has unnesserary bound check (#992)
    Solution:
    - small refactor

--- memiavl/layout_little_endian.go
+++ memiavl/layout_little_endian.go
@@ -16,35 +16,36 @@ func NewNodes(data []byte) (Nodes, error) {
 	return Nodes{data}, nil
 }
 
-func (nodes Nodes) Node(i uint32) *NodeLayout {
-	return &NodeLayout{data: nodes.data[i*SizeNode:]}
+func (nodes Nodes) Node(i uint32) NodeLayout {
+	offset := int(i) * SizeNode
+	return NodeLayout{data: (*[SizeNode]byte)(nodes.data[offset : offset+SizeNode])}
 }
 
 // see comment of `PersistedNode`
 type NodeLayout struct {
-	data []byte
+	data *[SizeNode]byte
 }
 
-func (node *NodeLayout) Height() uint8 {
+func (node NodeLayout) Height() uint8 {
 	return node.data[OffsetHeight]
 }
 
-func (node *NodeLayout) Version() uint32 {
+func (node NodeLayout) Version() uint32 {
 	return binary.LittleEndian.Uint32(node.data[OffsetVersion : OffsetVersion+4])
 }
 
-func (node *NodeLayout) Size() uint32 {
+func (node NodeLayout) Size() uint32 {
 	return binary.LittleEndian.Uint32(node.data[OffsetSize : OffsetSize+4])
 }
 
-func (node *NodeLayout) KeyOffset() uint64 {
+func (node NodeLayout) KeyOffset() uint64 {
 	return binary.LittleEndian.Uint64(node.data[OffsetKeyOffset : OffsetKeyOffset+8])
 }
 
-func (node *NodeLayout) KeyNode() uint32 {
+func (node NodeLayout) KeyNode() uint32 {
 	return binary.LittleEndian.Uint32(node.data[OffsetKeyNode : OffsetKeyNode+4])
 }
 
-func (node *NodeLayout) Hash() []byte {
+func (node NodeLayout) Hash() []byte {
 	return node.data[OffsetHash : OffsetHash+SizeHash]
 }
--- memiavl/layout_native.go
+++ memiavl/layout_native.go
@@ -8,58 +8,57 @@ import (
 	"unsafe"
 )
 
+type NodeLayout = *nodeLayout
+
 // Nodes is a continuously stored IAVL nodes
 type Nodes struct {
-	nodes []NodeLayout
+	nodes []nodeLayout
 }
 
 func NewNodes(buf []byte) (Nodes, error) {
 	// check alignment and size of the buffer
 	p := unsafe.Pointer(unsafe.SliceData(buf))
-	if uintptr(p)%unsafe.Alignof(NodeLayout{}) != 0 {
+	if uintptr(p)%unsafe.Alignof(nodeLayout{}) != 0 {
 		return Nodes{}, errors.New("input buffer is not aligned")
 	}
-	size := int(unsafe.Sizeof(NodeLayout{}))
+	size := int(unsafe.Sizeof(nodeLayout{}))
 	if len(buf)%size != 0 {
 		return Nodes{}, errors.New("input buffer length is not correct")
 	}
-	nodes := unsafe.Slice((*NodeLayout)(p), len(buf)/size)
+	nodes := unsafe.Slice((*nodeLayout)(p), len(buf)/size)
 	return Nodes{nodes}, nil
 }
 
-func (nodes Nodes) Node(i uint32) *NodeLayout {
+func (nodes Nodes) Node(i uint32) NodeLayout {
 	return &nodes.nodes[i]
 }
 
 // see comment of `PersistedNode`
-type NodeLayout struct {
+type nodeLayout struct {
 	data [4]uint32
 	hash [32]byte
 }
 
-func (node *NodeLayout) Height() uint8 {
+func (node *nodeLayout) Height() uint8 {
 	return uint8(node.data[0])
 }
 
-func (node *NodeLayout) Version() uint32 {
+func (node *nodeLayout) Version() uint32 {
 	return node.data[1]
 }
 
-func (node *NodeLayout) Size() uint32 {
-	if node.Height() == 0 {
-		return 1
-	}
+func (node *nodeLayout) Size() uint32 {
 	return node.data[2]
 }
 
-func (node *NodeLayout) KeyNode() uint32 {
+func (node *nodeLayout) KeyNode() uint32 {
 	return node.data[3]
 }
 
-func (node *NodeLayout) KeyOffset() uint64 {
+func (node *nodeLayout) KeyOffset() uint64 {
 	return uint64(node.data[2]) | uint64(node.data[3])<<32
 }
 
-func (node *NodeLayout) Hash() []byte {
+func (node *nodeLayout) Hash() []byte {
 	return node.hash[:]
 }
