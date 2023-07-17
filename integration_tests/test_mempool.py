adc84dd65c16f77eabdf7cf90ef0a2396c8ab7fd
yihuang <huang@crypto.com>
2023-07-17 18:48:41 +0800

    Update integration_tests/test_mempool.py
    Co-authored-by: mmsqe <mavis@crypto.com>
    Signed-off-by: yihuang <huang@crypto.com>

--- integration_tests/test_mempool.py
+++ integration_tests/test_mempool.py
@@ -76,6 +76,6 @@ def test_mempool(cronos_mempool):
 
 def test_blocked_address(cronos_mempool):
     cli = cronos_mempool.cosmos_cli(0)
-    rsp = cli.transfer("signer1", "validator", "1basecro")
+    rsp = cli.transfer("signer1", cli.address("validator"), "1basecro")
     assert rsp["code"] != 0
     assert "signer is blocked" in rsp["raw_log"]
