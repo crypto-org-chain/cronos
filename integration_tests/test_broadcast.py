1bbf3afe7e1c00a7b131a6167446824d16b67ea3
mmsqe <mavis@crypto.com>
2023-07-25 17:59:49 +0800

    Problem: block mode is deprecated, need implement in client side (#1103)
    * update pystarport
    
    * add event_query_tx_for
    
    * add change doc

---
+++ integration_tests/test_broadcast.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+import pytest
+
+from .network import setup_custom_cronos
+from .utils import submit_any_proposal
+
+
+@pytest.fixture(scope="module")
+def custom_cronos(tmp_path_factory):
+    path = tmp_path_factory.mktemp("cronos")
+    yield from setup_custom_cronos(
+        path, 26400, Path(__file__).parent / "configs/broadcast.jsonnet"
+    )
+
+
+def test_submit_any_proposal(custom_cronos, tmp_path):
+    submit_any_proposal(custom_cronos, tmp_path, True)
