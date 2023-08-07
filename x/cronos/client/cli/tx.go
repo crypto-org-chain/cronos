da6ccdb1f8004dfc6ff898ea3b2fb17f0b6713a5
mmsqe <mavis@crypto.com>
2023-08-07 09:43:37 +0800

    Problem: feeibc, evm and feemarket gensis migrate is broken

--- x/cronos/client/cli/tx.go
+++ x/cronos/client/cli/tx.go
@@ -2,6 +2,7 @@ package cli
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"strconv"
@@ -18,9 +19,14 @@ import (
 
 	"github.com/spf13/cobra"
 
+	errorsmod "cosmossdk.io/errors"
 	"github.com/cosmos/cosmos-sdk/client"
-
+	genutiltypes "github.com/cosmos/cosmos-sdk/x/genutil/types"
+	ibcfeetypes "github.com/cosmos/ibc-go/v6/modules/apps/29-fee/types"
 	"github.com/crypto-org-chain/cronos/v2/x/cronos/types"
+	evmtypes "github.com/evmos/ethermint/x/evm/types"
+	feemarkettypes "github.com/evmos/ethermint/x/feemarket/types"
+	tmjson "github.com/tendermint/tendermint/libs/json"
 	rpchttp "github.com/tendermint/tendermint/rpc/client/http"
 	coretypes "github.com/tendermint/tendermint/rpc/core/types"
 	tmtypes "github.com/tendermint/tendermint/types"
@@ -44,6 +50,7 @@ func GetTxCmd() *cobra.Command {
 	cmd.AddCommand(CmdTurnBridge())
 	cmd.AddCommand(CmdUpdatePermissions())
 	cmd.AddCommand(EventQueryTxFor())
+	cmd.AddCommand(MigrateGenesisCmd())
 
 	return cmd
 }
@@ -367,6 +374,176 @@ func EventQueryTxFor() *cobra.Command {
 	}
 
 	flags.AddTxFlagsToCmd(cmd)
+
+	return cmd
+}
+
+type ExportEvmGenesisState struct {
+	evmtypes.GenesisState
+	Params ExportEvmParams `json:"params"`
+}
+
+type ExportEvmParams struct {
+	evmtypes.Params
+	ExtraEIPs []string `json:"extra_eips"`
+}
+
+type ExportFeemarketGenesisState struct {
+	feemarkettypes.GenesisState
+	Params   ExportFeemarketParams `json:"params"`
+	BlockGas uint64                `json:"block_gas,string"`
+}
+
+type ExportFeemarketParams struct {
+	feemarkettypes.Params
+	EnableHeight int64 `json:"enable_height,string"`
+}
+
+func Migrate(appState genutiltypes.AppMap, clientCtx client.Context) genutiltypes.AppMap {
+	// Add feeibc with default genesis.
+	if appState[ibcfeetypes.ModuleName] == nil {
+		appState[ibcfeetypes.ModuleName] = clientCtx.Codec.MustMarshalJSON(ibcfeetypes.DefaultGenesisState())
+	}
+	var evmState ExportEvmGenesisState
+	err := json.Unmarshal(appState[evmtypes.ModuleName], &evmState)
+	if err != nil {
+		panic(err)
+	}
+	data, err := json.Marshal(evmState)
+	if err != nil {
+		panic(err)
+	}
+	appState[evmtypes.ModuleName] = data
+
+	var feemarketState ExportFeemarketGenesisState
+	err = json.Unmarshal(appState[feemarkettypes.ModuleName], &feemarketState)
+	if err != nil {
+		panic(err)
+	}
+	feemarketState.GenesisState.BlockGas = feemarketState.BlockGas
+	data, err = json.Marshal(feemarketState)
+	if err != nil {
+		panic(err)
+	}
+	appState[feemarkettypes.ModuleName] = data
+	return appState
+}
+
+const (
+	flagGenesisTime   = "genesis-time"
+	chainUpgradeGuide = "https://github.com/cosmos/cosmos-sdk/blob/main/UPGRADING.md"
+)
+
+// validateGenDoc reads a genesis file and validates that it is a correct
+// Tendermint GenesisDoc. This function does not do any cosmos-related
+// validation.
+func validateGenDoc(importGenesisFile string) (*tmtypes.GenesisDoc, error) {
+	genDoc, err := tmtypes.GenesisDocFromFile(importGenesisFile)
+	if err != nil {
+		return nil, fmt.Errorf("%s. Make sure that"+
+			" you have correctly migrated all Tendermint consensus params, please see the"+
+			" chain migration guide at %s for more info",
+			err.Error(), chainUpgradeGuide,
+		)
+	}
+
+	return genDoc, nil
+}
+
+// migrationMap is a map of SDK versions to their respective genesis migration functions.
+var migrationMap = genutiltypes.MigrationMap{
+	"v1.0": Migrate,
+}
+
+// GetMigrationCallback returns a MigrationCallback for a given version.
+func GetMigrationCallback(version string) genutiltypes.MigrationCallback {
+	return migrationMap[version]
+}
+
+// MigrateGenesisCmd returns a command to execute genesis state migration.
+func MigrateGenesisCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "migrate [target-version] [genesis-file]",
+		Short: "Migrate genesis to a specified target version",
+		Long: fmt.Sprintf(`Migrate the source genesis into the target version and print to STDOUT.
+
+Example:
+$ %s migrate v1.0 /path/to/genesis.json --chain-id=cronos_777-1 --genesis-time=2019-04-22T17:00:00Z
+`, version.AppName),
+		Args: cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx := client.GetClientContextFromCmd(cmd)
+
+			var err error
+
+			target := args[0]
+			importGenesis := args[1]
+
+			genDoc, err := validateGenDoc(importGenesis)
+			if err != nil {
+				return err
+			}
+
+			// Since some default values are valid values, we just print to
+			// make sure the user didn't forget to update these values.
+			if genDoc.ConsensusParams.Evidence.MaxBytes == 0 {
+				fmt.Printf("Warning: consensus_params.evidence.max_bytes is set to 0. If this is"+
+					" deliberate, feel free to ignore this warning. If not, please have a look at the chain"+
+					" upgrade guide at %s.\n", chainUpgradeGuide)
+			}
+
+			var initialState genutiltypes.AppMap
+			if err := json.Unmarshal(genDoc.AppState, &initialState); err != nil {
+				return errorsmod.Wrap(err, "failed to JSON unmarshal initial genesis state")
+			}
+
+			migrationFunc := GetMigrationCallback(target)
+			if migrationFunc == nil {
+				return fmt.Errorf("unknown migration function for version: %s", target)
+			}
+
+			// TODO: handler error from migrationFunc call
+			newGenState := migrationFunc(initialState, clientCtx)
+
+			genDoc.AppState, err = json.Marshal(newGenState)
+			if err != nil {
+				return errorsmod.Wrap(err, "failed to JSON marshal migrated genesis state")
+			}
+
+			genesisTime, _ := cmd.Flags().GetString(flagGenesisTime)
+			if genesisTime != "" {
+				var t time.Time
+
+				err := t.UnmarshalText([]byte(genesisTime))
+				if err != nil {
+					return errorsmod.Wrap(err, "failed to unmarshal genesis time")
+				}
+
+				genDoc.GenesisTime = t
+			}
+
+			chainID, _ := cmd.Flags().GetString(flags.FlagChainID)
+			if chainID != "" {
+				genDoc.ChainID = chainID
+			}
+
+			bz, err := tmjson.Marshal(genDoc)
+			if err != nil {
+				return errorsmod.Wrap(err, "failed to marshal genesis doc")
+			}
+
+			sortedBz, err := sdk.SortJSON(bz)
+			if err != nil {
+				return errorsmod.Wrap(err, "failed to sort JSON genesis doc")
+			}
+
+			cmd.Println(string(sortedBz))
+			return nil
+		},
+	}
+
+	cmd.Flags().String(flagGenesisTime, "", "override genesis_time with this flag")
+	cmd.Flags().String(flags.FlagChainID, "", "override chain_id with this flag")
 
 	return cmd
 }
