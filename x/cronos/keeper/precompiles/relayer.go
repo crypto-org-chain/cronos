3ca158add92b31d5c354722ca22d67ab419ecd3a
HuangYi <huang@crypto.com>
2023-09-11 16:35:15 +0800

    Problem: precompile don't validate the caller
    Solution:
    - validate the caller is the signer in the message

--- x/cronos/keeper/precompiles/relayer.go
+++ x/cronos/keeper/precompiles/relayer.go
@@ -77,16 +77,30 @@ const (
 	prefixTimeoutOnClose
 )
 
+type NativeMessage interface {
+	codec.ProtoMarshaler
+	GetSigners() []sdk.AccAddress
+}
+
 // exec is a generic function that executes the given action in statedb, and marshal/unmarshal the input/output
-func exec[Req codec.ProtoMarshaler, Resp codec.ProtoMarshaler](
-	bc *RelayerContract,
+func exec[Req NativeMessage, Resp codec.ProtoMarshaler](
+	cdc codec.Codec,
 	stateDB precompiles.ExtStateDB,
+	caller common.Address,
 	input []byte,
 	msg Req,
 	action func(context.Context, Req) (Resp, error),
 ) ([]byte, error) {
-	if err := bc.cdc.Unmarshal(input, msg); err != nil {
+	if err := cdc.Unmarshal(input, msg); err != nil {
 		return nil, fmt.Errorf("fail to Unmarshal %T", msg)
+	}
+
+	signers := msg.GetSigners()
+	if len(signers) != 1 {
+		return nil, errors.New("don't support multi-signers message")
+	}
+	if common.BytesToAddress(signers[0].Bytes()) != caller {
+		return nil, errors.New("caller is not authenticated")
 	}
 
 	var res Resp
@@ -98,7 +112,7 @@ func exec[Req codec.ProtoMarshaler, Resp codec.ProtoMarshaler](
 		return nil, err
 	}
 
-	return bc.cdc.Marshal(res)
+	return cdc.Marshal(res)
 }
 
 func (bc *RelayerContract) Run(evm *vm.EVM, contract *vm.Contract, readonly bool) ([]byte, error) {
@@ -119,37 +133,37 @@ func (bc *RelayerContract) Run(evm *vm.EVM, contract *vm.Contract, readonly bool
 	)
 	switch prefix {
 	case prefixCreateClient:
-		res, err = exec(bc, stateDB, input, new(clienttypes.MsgCreateClient), bc.ibcKeeper.CreateClient)
+		res, err = exec(bc.cdc, stateDB, contract.CallerAddress, input, new(clienttypes.MsgCreateClient), bc.ibcKeeper.CreateClient)
 	case prefixUpdateClient:
-		res, err = exec(bc, stateDB, input, new(clienttypes.MsgUpdateClient), bc.ibcKeeper.UpdateClient)
+		res, err = exec(bc.cdc, stateDB, contract.CallerAddress, input, new(clienttypes.MsgUpdateClient), bc.ibcKeeper.UpdateClient)
 	case prefixUpgradeClient:
-		res, err = exec(bc, stateDB, input, new(clienttypes.MsgUpgradeClient), bc.ibcKeeper.UpgradeClient)
+		res, err = exec(bc.cdc, stateDB, contract.CallerAddress, input, new(clienttypes.MsgUpgradeClient), bc.ibcKeeper.UpgradeClient)
 	case prefixSubmitMisbehaviour:
-		res, err = exec(bc, stateDB, input, new(clienttypes.MsgSubmitMisbehaviour), bc.ibcKeeper.SubmitMisbehaviour)
+		res, err = exec(bc.cdc, stateDB, contract.CallerAddress, input, new(clienttypes.MsgSubmitMisbehaviour), bc.ibcKeeper.SubmitMisbehaviour)
 	case prefixConnectionOpenInit:
-		res, err = exec(bc, stateDB, input, new(conntypes.MsgConnectionOpenInit), bc.ibcKeeper.ConnectionOpenInit)
+		res, err = exec(bc.cdc, stateDB, contract.CallerAddress, input, new(conntypes.MsgConnectionOpenInit), bc.ibcKeeper.ConnectionOpenInit)
 	case prefixConnectionOpenTry:
-		res, err = exec(bc, stateDB, input, new(conntypes.MsgConnectionOpenTry), bc.ibcKeeper.ConnectionOpenTry)
+		res, err = exec(bc.cdc, stateDB, contract.CallerAddress, input, new(conntypes.MsgConnectionOpenTry), bc.ibcKeeper.ConnectionOpenTry)
 	case prefixConnectionOpenAck:
-		res, err = exec(bc, stateDB, input, new(conntypes.MsgConnectionOpenAck), bc.ibcKeeper.ConnectionOpenAck)
+		res, err = exec(bc.cdc, stateDB, contract.CallerAddress, input, new(conntypes.MsgConnectionOpenAck), bc.ibcKeeper.ConnectionOpenAck)
 	case prefixConnectionOpenConfirm:
-		res, err = exec(bc, stateDB, input, new(conntypes.MsgConnectionOpenConfirm), bc.ibcKeeper.ConnectionOpenConfirm)
+		res, err = exec(bc.cdc, stateDB, contract.CallerAddress, input, new(conntypes.MsgConnectionOpenConfirm), bc.ibcKeeper.ConnectionOpenConfirm)
 	case prefixChannelOpenInit:
-		res, err = exec(bc, stateDB, input, new(chantypes.MsgChannelOpenInit), bc.ibcKeeper.ChannelOpenInit)
+		res, err = exec(bc.cdc, stateDB, contract.CallerAddress, input, new(chantypes.MsgChannelOpenInit), bc.ibcKeeper.ChannelOpenInit)
 	case prefixChannelOpenTry:
-		res, err = exec(bc, stateDB, input, new(chantypes.MsgChannelOpenTry), bc.ibcKeeper.ChannelOpenTry)
+		res, err = exec(bc.cdc, stateDB, contract.CallerAddress, input, new(chantypes.MsgChannelOpenTry), bc.ibcKeeper.ChannelOpenTry)
 	case prefixChannelOpenAck:
-		res, err = exec(bc, stateDB, input, new(chantypes.MsgChannelOpenAck), bc.ibcKeeper.ChannelOpenAck)
+		res, err = exec(bc.cdc, stateDB, contract.CallerAddress, input, new(chantypes.MsgChannelOpenAck), bc.ibcKeeper.ChannelOpenAck)
 	case prefixChannelOpenConfirm:
-		res, err = exec(bc, stateDB, input, new(chantypes.MsgChannelOpenConfirm), bc.ibcKeeper.ChannelOpenConfirm)
+		res, err = exec(bc.cdc, stateDB, contract.CallerAddress, input, new(chantypes.MsgChannelOpenConfirm), bc.ibcKeeper.ChannelOpenConfirm)
 	case prefixRecvPacket:
-		res, err = exec(bc, stateDB, input, new(chantypes.MsgRecvPacket), bc.ibcKeeper.RecvPacket)
+		res, err = exec(bc.cdc, stateDB, contract.CallerAddress, input, new(chantypes.MsgRecvPacket), bc.ibcKeeper.RecvPacket)
 	case prefixAcknowledgement:
-		res, err = exec(bc, stateDB, input, new(chantypes.MsgAcknowledgement), bc.ibcKeeper.Acknowledgement)
+		res, err = exec(bc.cdc, stateDB, contract.CallerAddress, input, new(chantypes.MsgAcknowledgement), bc.ibcKeeper.Acknowledgement)
 	case prefixTimeout:
-		res, err = exec(bc, stateDB, input, new(chantypes.MsgTimeout), bc.ibcKeeper.Timeout)
+		res, err = exec(bc.cdc, stateDB, contract.CallerAddress, input, new(chantypes.MsgTimeout), bc.ibcKeeper.Timeout)
 	case prefixTimeoutOnClose:
-		res, err = exec(bc, stateDB, input, new(chantypes.MsgTimeoutOnClose), bc.ibcKeeper.TimeoutOnClose)
+		res, err = exec(bc.cdc, stateDB, contract.CallerAddress, input, new(chantypes.MsgTimeoutOnClose), bc.ibcKeeper.TimeoutOnClose)
 	default:
 		return nil, errors.New("unknown method")
 	}
