4f2f146d679673c296cd70bf26e38030ca7897e3
jacksoom <lifengliu1994@gmail.com>
2023-09-18 12:54:23 +0800

    Problem: memiavl add workpool

--- memiavl/export.go
+++ memiavl/export.go
@@ -15,6 +15,8 @@ var ErrorExportDone = errors.New("export is complete")
 // especially since callers may export several IAVL stores in parallel (e.g. the Cosmos SDK).
 const exportBufferSize = 32
 
+const exportSnapshotWorkerNum = 32
+
 type MultiTreeExporter struct {
 	// only one of them is non-nil
 	db    *DB
@@ -31,9 +33,10 @@ func NewMultiTreeExporter(dir string, version uint32, supportExportNonSnapshotVe
 	)
 	if supportExportNonSnapshotVersion {
 		db, err = Load(dir, Options{
-			TargetVersion: version,
-			ZeroCopy:      true,
-			ReadOnly:      true,
+			TargetVersion:       version,
+			ZeroCopy:            true,
+			ReadOnly:            true,
+			SnapshotWriterLimit: exportSnapshotWorkerNum,
 		})
 		if err != nil {
 			return nil, fmt.Errorf("invalid height: %d, %w", version, err)
