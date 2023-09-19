4c922a8a1f2ef55e266fda2c7b7b3df1b38935ce
jonghwan lee <jonghwan@crypto.com>
2023-09-19 13:24:51 +0900

    Problem: not supporting graceful close (fix #1173)
    wait a little before shutdown
    
    Update cmd/cronosd/main.go
    
    thanks
    
    Co-authored-by: mmsqe <mavis@crypto.com>
    Signed-off-by: Jongwhan Lee <51560997+leejw51crypto@users.noreply.github.com>
    
    change comment
    
    reduce waiting time

--- cmd/cronosd/main.go
+++ cmd/cronosd/main.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
 	"github.com/crypto-org-chain/cronos/v2/app"
@@ -9,6 +13,23 @@ import (
 )
 
 func main() {
+	// Create a channel to listen for incoming signals
+	sigChan := make(chan os.Signal, 1)
+
+	// Notify the channel for SIGINT and SIGTERM signals
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	// Start a goroutine to handle the signal
+	go func() {
+		sig := <-sigChan
+		fmt.Printf("Received signal: %s\n", sig)
+		// Perform cleanup here
+		fmt.Println("Performing cleanup...")
+		time.Sleep(1 * time.Second) // wait for other handlers to finish
+		fmt.Println("Cleanup done!")
+		os.Exit(1)
+	}()
+
 	rootCmd, _ := cmd.NewRootCmd()
 	if err := svrcmd.Execute(rootCmd, cmd.EnvPrefix, app.DefaultNodeHome); err != nil {
 		os.Exit(1)
