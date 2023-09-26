629eed9a7c940e62b3ff16bd097a0c176d9ad977
mmsqe <mavis@crypto.com>
2023-09-26 14:46:20 +0800

    use cb events

--- x/cronos/events/bindings/src/ICA.sol
+++ x/cronos/events/bindings/src/ICA.sol
@@ -3,6 +3,29 @@ pragma solidity ^0.8.4;
 
 interface IICAModule {
     event SubmitMsgsResult(uint64 seq);
+    event DestinationCallback(
+        string module,
+        string callbackType,
+        string callbackAddress,
+        string callbackResult,
+        string callbackError,
+        string callbackExecGasLimit,
+        string callbackCommitGasLimit,
+        string packetDestPort,
+        string packetDestChannel,
+        string packetSequence
+    );
+    event SourceCallback(
+        string module,
+        string callbackType,
+        string callbackAddress,
+        string callbackResult,
+        string callbackExecGasLimit,
+        string callbackCommitGasLimit,
+        string packetSrcPort,
+        string packetSrcChannel,
+        string packetSequence
+    );
     function registerAccount(string calldata connectionID, string calldata version) external payable returns (bool);
     function queryAccount(string calldata connectionID, address addr) external view returns (string memory);
     function submitMsgs(string calldata connectionID, bytes calldata data, uint256 timeout) external payable returns (uint64);
