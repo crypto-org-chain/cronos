6b80a9d20db1b0040294b3ce48312800be6155c6
mmsqe <mavis@crypto.com>
2023-10-27 14:39:41 +0800

    Apply suggestions from code review

--- integration_tests/test_client_id.py
+++ integration_tests/test_client_id.py
@@ -14,18 +14,19 @@ def test_config_client_id(cronos):
     cli = cronos.cosmos_cli(0)
     dir = cli.data_dir / "config"
 
-    def edit_gensis_cfg(chain_id):
+    def edit_genesis_cfg(chain_id):
         genesis_cfg = dir / "genesis.json"
         genesis = json.loads(genesis_cfg.read_text())
         genesis["chain_id"] = chain_id
         genesis_cfg.write_text(json.dumps(genesis))
 
     # start with empty chain_id from genesis should fail
-    edit_gensis_cfg("")
+    edit_genesis_cfg("")
     with pytest.raises(Exception):
         cronos.supervisorctl("start", n0)
 
-    edit_gensis_cfg("cronos_777-1")
+    chain_id = 777
+    edit_genesis_cfg(f"cronos_{chain_id}-1")
     cronos.supervisorctl("start", n0)
     wait_for_port(ports.evmrpc_port(p0))
-    assert w3.eth.chain_id == 777
+    assert w3.eth.chain_id == chain_id
