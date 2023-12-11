4436be6a03581afd1777e875b70b772a4d6b2efe
HuangYi <huang@crypto.com>
2023-12-11 11:41:33 +0800

    fix unit test

--- app/ante/interface.go
+++
@@ -1,7 +0,0 @@
-package ante
-
-import sdk "github.com/cosmos/cosmos-sdk/types"
-
-type CronosKeeper interface {
-	HasPermission(ctx sdk.Context, account sdk.AccAddress, permissionsToCheck uint64) bool
-}
