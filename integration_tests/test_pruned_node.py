18462e2960ad649e766a018b361cd2e63f4b7045
HuangYi <huang@crypto.com>
2023-12-22 14:46:40 +0800

    fix test

--- integration_tests/test_pruned_node.py
+++ integration_tests/test_pruned_node.py
@@ -46,7 +46,7 @@ def test_pruned_node(cronos):
     )
     signed = sign_transaction(w3, tx, KEYS["validator"])
     txhash = w3.eth.send_raw_transaction(signed.rawTransaction)
-    exp_gas_used = 51383
+    exp_gas_used = 51384
 
     print("wait for prunning happens")
     wait_for_new_blocks(cronos.cosmos_cli(0), 10)
