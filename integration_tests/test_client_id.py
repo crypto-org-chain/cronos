1a1d1ed14748605b791c433b24c29a9e09b498e2
mmsqe <mavis@crypto.com>
2024-02-20 09:11:42 +0800

    chain id load from blockstore before genesis
    for more info:
    https://github.com/cosmos/cosmos-sdk/pull/18920/files

--- integration_tests/test_client_id.py
+++ integration_tests/test_client_id.py
@@ -1,6 +1,5 @@
 import json
 
-import pytest
 from pystarport import ports
 
 from .utils import wait_for_port
@@ -14,19 +13,15 @@ def test_config_client_id(cronos):
     cli = cronos.cosmos_cli(0)
     dir = cli.data_dir / "config"
 
-    def edit_genesis_cfg(chain_id):
+    def assert_chain_id(chain_id):
         genesis_cfg = dir / "genesis.json"
         genesis = json.loads(genesis_cfg.read_text())
-        genesis["chain_id"] = chain_id
+        genesis["chain_id"] = f"cronos_{chain_id}-1"
         genesis_cfg.write_text(json.dumps(genesis))
-
-    # start with empty chain_id from genesis should fail
-    edit_genesis_cfg("")
-    with pytest.raises(Exception):
         cronos.supervisorctl("start", n0)
+        wait_for_port(ports.evmrpc_port(p0))
+        assert w3.eth.chain_id == chain_id
 
-    chain_id = 777
-    edit_genesis_cfg(f"cronos_{chain_id}-1")
-    cronos.supervisorctl("start", n0)
-    wait_for_port(ports.evmrpc_port(p0))
-    assert w3.eth.chain_id == chain_id
+    assert_chain_id(776)
+    cronos.supervisorctl("stop", n0)
+    assert_chain_id(777)
