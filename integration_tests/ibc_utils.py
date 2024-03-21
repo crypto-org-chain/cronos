a7b3cc18888c86b0e60e00e12c848b65502a80a6
mmsqe <mavis@crypto.com>
2024-03-21 10:10:56 +0800

    cleanup

--- integration_tests/ibc_utils.py
+++ integration_tests/ibc_utils.py
@@ -471,16 +471,8 @@ def ibc_incentivized_transfer(ibc):
     ], actual
     path = f"transfer/{dst_channel}/{base_denom}"
     denom_hash = hashlib.sha256(path.encode()).hexdigest().upper()
-    assert json.loads(
-        chains[0].raw(
-            "query",
-            "ibc-transfer",
-            "denom-trace",
-            denom_hash,
-            node=ibc.chainmain.node_rpc(0),
-            output="json",
-        )
-    )["denom_trace"] == {"path": f"transfer/{dst_channel}", "base_denom": base_denom}
+    denom_trace = chains[0].ibc_denom_trace(path, ibc.chainmain.node_rpc(0))
+    assert denom_trace == {"path": f"transfer/{dst_channel}", "base_denom": base_denom}
     assert get_balances(ibc.chainmain, receiver) == [
         {"denom": "basecro", "amount": f"{old_amt_receiver_base}"},
         {"denom": f"ibc/{denom_hash}", "amount": f"{amount}"},
