90864494d7508e39e2e1d1809e5296934ecbae7b
yihuang <huang@crypto.com>
2024-04-26 15:44:16 +0800

    Update x/e2ee/types/codec.go
    Co-authored-by: mmsqe <mavis@crypto.com>
    Signed-off-by: yihuang <huang@crypto.com>

--- x/e2ee/types/codec.go
+++ x/e2ee/types/codec.go
@@ -7,7 +7,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/types/msgservice"
 )
 
-// RegisterLegacyAminoCodec registers the necessary x/bank interfaces and concrete types
+// RegisterLegacyAminoCodec registers the necessary x/e2ee interfaces and concrete types
 // on the provided LegacyAmino codec. These types are used for Amino JSON serialization.
 func RegisterLegacyAminoCodec(_ *codec.LegacyAmino) {}
 
