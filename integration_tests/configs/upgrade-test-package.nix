8f36f0272a51fa52280c8df01c676b2faf903520
mmsqe <mavis@crypto.com>
2024-04-30 15:36:02 +0800

    cleanup

--- integration_tests/configs/upgrade-test-package.nix
+++ integration_tests/configs/upgrade-test-package.nix
@@ -7,11 +7,11 @@ let
       shortRev = builtins.substring 0 7 rev;
     };
   }).defaultNix;
-  # v0.8.0
+  # v0.8.x
   releasedGenesis = (fetchFlake "crypto-org-chain/cronos" "2f2cc88b501b47149690fdef05afbbbe5bc116c9").default;
-  # v1.0.15 a85daedc6ffc
+  # v1.0.15
   released1_1 = (fetchFlake "crypto-org-chain/cronos" "1f5e2618362303d91f621b47cbc1115cf4fa0195").default;
-  # v1.1.1 5b153a460e7a
+  # v1.1.1
   released1_2 = (fetchFlake "crypto-org-chain/cronos" "10b8eeb9052e3c52aa59dec15f5d3aca781d1271").default;
   current = pkgs.callPackage ../../. { };
 in
