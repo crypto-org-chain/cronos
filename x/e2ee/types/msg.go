3aa9d02c0331231762dd403476b5ae0b28e8d840
yihuang <yi.codeplayer@gmail.com>
2024-05-03 10:50:44 +0800

    cleanup

--- x/e2ee/types/msg.go
+++ x/e2ee/types/msg.go
@@ -8,19 +8,14 @@ import (
 )
 
 func (m *MsgRegisterEncryptionKey) ValidateBasic() error {
-	if m.Address == "" {
-		return fmt.Errorf("address cannot be empty")
-	}
-	if len(m.Key) == 0 {
-		return fmt.Errorf("key cannot be empty")
-	}
-
-	if _, err := age.ParseX25519Recipient(m.Key); err != nil {
-		return err
-	}
 	// validate bech32 format of Address
 	if _, err := sdk.AccAddressFromBech32(m.Address); err != nil {
 		return fmt.Errorf("invalid address: %s", err)
 	}
-	return nil
+	return ValidateRecipientKey(m.Key)
+}
+
+func ValidateRecipientKey(key string) error {
+	_, err := age.ParseX25519Recipient(key)
+	return err
 }
