c315b98335f1fa11da2c8bf18fdbcd963079ea9f
huangyi <huang@crypto.com>
2024-05-09 11:04:36 +0800

    Problem: no cluster benchmarking
    - Integrate testground
    - Implement python sdk for testground

---
+++ testground/benchmark/benchmark/main.py
@@ -0,0 +1,44 @@
+import json
+import os
+import queue
+
+from .context import Context
+from .network import get_data_ip
+
+
+def influxdb_url():
+    return os.environ.get("INFLUXDB_URL", "http://testground-influxdb:8086")
+
+
+def entrypoint(ctx: Context):
+    ctx.init_common()
+
+    print("global_seq:", ctx.global_seq, "group_seq:", ctx.group_seq)
+
+    # share the peer addresses
+    q = queue.Queue()
+    addr = get_data_ip(ctx.params)
+    ctx.sync.publish_subscribe("peers", str(addr), q.put)
+    peers = {q.get() for i in range(ctx.params.test_instance_count)}
+    if len(peers) != ctx.params.test_instance_count:
+        ctx.record_failure("peer addresses are not unique")
+        ctx.sync.signal_and_wait("failed", ctx.params.test_instance_count)
+        return
+
+    print("peers", peers)
+
+    ctx.record_success()
+
+
+TEST_CASES = {
+    "entrypoint": entrypoint,
+}
+
+
+def main():
+    with Context() as ctx:
+        TEST_CASES[ctx.params.test_case](ctx)
+
+
+if __name__ == "__main__":
+    main()
---
+++ testground/benchmark/benchmark/params.py
@@ -0,0 +1,107 @@
+import ipaddress
+import os
+from dataclasses import dataclass, fields
+from datetime import datetime
+
+
+@dataclass
+class RunParams:
+    test_branch: str = ""
+    test_case: str = ""
+    test_group_id: str = ""
+    test_group_instance_count: int = 0
+    test_instance_count: int = 0
+    test_instance_params: str = ""
+    test_instance_role: str = ""
+    test_outputs_path: str = ""
+    test_plan: str = ""
+    test_repo: str = ""
+    test_run: str = ""
+    test_sidecar: bool = False
+    test_start_time: datetime = None
+    test_subnet: ipaddress.IPv4Network = None
+    test_tag: str = ""
+    test_capture_profiles: str = ""
+    test_temp_path: str = ""
+    log_level: str = ""
+
+    def events_key(self):
+        return (
+            f"run:{self.test_run}"
+            f":plan:{self.test_plan}"
+            f":case:{self.test_case}"
+            ":run_events"
+        )
+
+    def topic_key(self, topic: str):
+        return (
+            f"run:{self.test_run}"
+            f":plan:{self.test_plan}"
+            f":case:{self.test_case}"
+            f":topics:{topic}"
+        )
+
+    def state_key(self, name: str):
+        return (
+            f"run:{self.test_run}"
+            f":plan:{self.test_plan}"
+            f":case:{self.test_case}"
+            f":states:{name}"
+        )
+
+    def ipaddress(self, seq: int) -> ipaddress.IPv4Address:
+        # add 256 to avoid conflict with system services
+        return self.test_subnet.network_address + (seq + 256)
+
+    def network_config(
+        self, global_seq: int, callback_state="network-configured"
+    ) -> dict:
+        """
+        config ip address based on global seq
+        """
+        return {
+            "network": "default",
+            "enable": True,
+            # using the assigned `GlobalSequencer` id per each of instance
+            # to fill in the last 2 octets of the new IP address for the instance
+            "IPv4": str(self.ipaddress(global_seq)) + "/16",
+            "IPv6": None,
+            "rules": None,
+            "default": {
+                "latency": 0,
+                "jitter": 0,
+                "bandwidth": 0,
+                "filter": 0,
+                "loss": 0,
+                "corrupt": 0,
+                "corrupt_corr": 0,
+                "reorder": 0,
+                "reorder_corr": 0,
+                "duplicate": 0,
+                "duplicate_corr": 0,
+            },
+            "callback_state": callback_state,
+            "routing_policy": "allow_all",
+        }
+
+
+def run_params(env=None) -> RunParams:
+    if env is None:
+        env = os.environ
+    p = RunParams()
+    for f in fields(RunParams):
+        value = env.get(f.name.upper())
+        if value is None:
+            continue
+        if f.type == bool:
+            value = convert_bool(value)
+        elif f.type == datetime:
+            value = datetime.fromisoformat(value)
+        else:
+            value = f.type(value)
+        setattr(p, f.name, value)
+    return p
+
+
+def convert_bool(s: str) -> bool:
+    return s == "true"
