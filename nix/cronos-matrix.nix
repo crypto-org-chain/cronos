37a7fdc92853f5a7c61638c84c021360ad0aca1e
huangyi <huang@crypto.com>
2024-05-23 15:02:58 +0800

    cleanup

--- nix/cronos-matrix.nix
+++ nix/cronos-matrix.nix
@@ -1,11 +1,10 @@
 { lib
 , stdenv
 , callPackage
-, pkgsBuildBuild
 , buildPackages
 , runCommand
-, rev ? "dirty"
 , bundle-exe
+, rev ? "dirty"
 }:
 let
   # make-tarball don't follow symbolic links to avoid duplicate file, the bundle should have no external references.
@@ -27,32 +26,31 @@ let
       "tarball" # tarball of the bundle, for distribution and checksum
     ];
   };
-  binaries = builtins.listToAttrs (builtins.map
-    ({ network, pkgtype }: {
-      name = builtins.concatStringsSep "-" (
-        [ "cronosd" ] ++
-        lib.optional (network != "mainnet") network ++
-        lib.optional (pkgtype != "nix") pkgtype
-      );
-      value =
-        let
-          cronosd = callPackage ../. {
-            inherit rev network;
-          };
-          bundle =
-            if stdenv.hostPlatform.isWindows then
-              bundle-win-exe cronosd
-            else
-              bundle-exe cronosd;
-        in
-        if pkgtype == "bundle" then
-          bundle
-        else if pkgtype == "tarball" then
-          make-tarball bundle
-        else
-          cronosd;
-    })
-    matrix
-  );
 in
-binaries
+builtins.listToAttrs (builtins.map
+  ({ network, pkgtype }: {
+    name = builtins.concatStringsSep "-" (
+      [ "cronosd" ] ++
+      lib.optional (network != "mainnet") network ++
+      lib.optional (pkgtype != "nix") pkgtype
+    );
+    value =
+      let
+        cronosd = callPackage ../. {
+          inherit rev network;
+        };
+        bundle =
+          if stdenv.hostPlatform.isWindows then
+            bundle-win-exe cronosd
+          else
+            bundle-exe cronosd;
+      in
+      if pkgtype == "bundle" then
+        bundle
+      else if pkgtype == "tarball" then
+        make-tarball bundle
+      else
+        cronosd;
+  })
+  matrix
+)
