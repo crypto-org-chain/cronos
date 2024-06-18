2fc0287138f78970d05ee35ab7439fcaadf3d7e2
mmsqe <mavis@crypto.com>
2024-06-18 09:42:04 +0800

    Problem: packed batch is not supported

--- integration_tests/test_ibc_rly_gas.py
+++ integration_tests/test_ibc_rly_gas.py
@@ -7,6 +7,7 @@ from .ibc_utils import (
     ibc_transfer,
     log_gas_records,
     prepare_network,
+    rly_transfer,
 )
 from .utils import wait_for_new_blocks
 
@@ -18,7 +19,7 @@ def ibc(request, tmp_path_factory):
     "prepare-network"
     name = request.param
     path = tmp_path_factory.mktemp(name)
-    yield from prepare_network(path, name, relayer=cluster.Relayer.HERMES.value)
+    yield from prepare_network(path, name, relayer=cluster.Relayer.RLY.value)
 
 
 records = []
@@ -28,13 +29,16 @@ def test_ibc(ibc):
     # chainmain-1 relayer -> cronos_777-1 signer2
     cli = ibc.cronos.cosmos_cli()
     wait_for_new_blocks(cli, 1)
-    ibc_transfer(ibc)
+    ibc_transfer(ibc, transfer_fn=rly_transfer)
+    # ibc_transfer(ibc)
     ibc_incentivized_transfer(ibc)
     ibc_multi_transfer(ibc)
     diff = 0.1
     record = log_gas_records(cli)
     if record:
+        print("mm-record", record)
         records.append(record)
     if len(records) == 2:
         res = float(sum(records[0]) / sum(records[1]))
+        print("mm-res", res)
         assert 1 - diff <= res <= 1 + diff, res
