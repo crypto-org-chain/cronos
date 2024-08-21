09bbf1f2d94501b0a2cd07d0d247a6e775b95d7b
mmsqe <mavis@crypto.com>
2024-08-21 09:26:14 +0800

    Revert "larger timeout"
    This reverts commit 33bce0bbaab0f8ce9e9f7e494a90038163027b93.

--- integration_tests/cosmoscli.py
+++ integration_tests/cosmoscli.py
@@ -1795,7 +1795,6 @@ class CosmosCLI:
                 "event-query-tx-for",
                 hash,
                 home=self.data_dir,
-                timeout="120s",
                 stderr=subprocess.DEVNULL,
             )
         )
