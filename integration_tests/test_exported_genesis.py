af4eed6cfc35a65862786523f16ea696ed6aafe6
mmsqe <mavis@crypto.com>
2024-08-29 21:26:04 +0800

    add test

--- integration_tests/test_exported_genesis.py
+++ integration_tests/test_exported_genesis.py
@@ -1,7 +1,10 @@
 import json
+import urllib.parse
 from pathlib import Path
 
 import pytest
+import requests
+from pystarport import ports
 
 from .network import setup_custom_cronos
 from .utils import ADDRS, CONTRACTS
@@ -34,3 +37,16 @@ def test_exported_token_mapping(custom_cronos):
     )
     assert rsp["contract"] == "0x68542BD12B41F5D51D6282Ec7D91D7d0D78E4503"
     assert rsp["auto_contract"] == "0x68542BD12B41F5D51D6282Ec7D91D7d0D78E4503"
+    denom = "ibc/6B5A664BF0AF4F71B2F0BAA33141E2F1321242FBD5D19762F541EC971ACB0865"
+    rsp = cli.query_contract_by_denom(denom)
+    expected = {
+        "contract": "0x0000000000000000000000000000000000000000",
+        "auto_contract": "",
+    }
+    assert rsp == expected
+    port = ports.api_port(custom_cronos.base_port(0))
+    param = urllib.parse.quote(denom, safe="")
+    param = urllib.parse.quote(param, safe="")
+    url = f"http://127.0.0.1:{port}/cronos/v1/contract_by_denom/{param}"
+    rsp = requests.get(url).json()
+    assert rsp == expected
