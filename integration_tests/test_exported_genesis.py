825c551121367acb347f6e9380253c788762b0b5
mmsqe <mavis@crypto.com>
2024-08-30 13:37:40 +0800

    Problem: url encoded characters are not properly handled in contract_by_denom (#1560)
    * Problem: url encoded characters are not properly handled in contract_by_denom
    
    * add test
    
    * update query str

--- integration_tests/test_exported_genesis.py
+++ integration_tests/test_exported_genesis.py
@@ -2,6 +2,8 @@ import json
 from pathlib import Path
 
 import pytest
+import requests
+from pystarport import ports
 
 from .network import setup_custom_cronos
 from .utils import ADDRS, CONTRACTS
@@ -29,8 +31,25 @@ def test_exported_contract(custom_cronos):
 
 def test_exported_token_mapping(custom_cronos):
     cli = custom_cronos.cosmos_cli(0)
-    rsp = cli.query_contract_by_denom(
-        "gravity0x0000000000000000000000000000000000000000"
-    )
-    assert rsp["contract"] == "0x68542BD12B41F5D51D6282Ec7D91D7d0D78E4503"
-    assert rsp["auto_contract"] == "0x68542BD12B41F5D51D6282Ec7D91D7d0D78E4503"
+    port = ports.api_port(custom_cronos.base_port(0))
+    for case in [
+        {
+            "denom": "gravity0x0000000000000000000000000000000000000000",
+            "expected": {
+                "contract": "0x68542BD12B41F5D51D6282Ec7D91D7d0D78E4503",
+                "auto_contract": "0x68542BD12B41F5D51D6282Ec7D91D7d0D78E4503",
+            },
+        },
+        {
+            "denom": "ibc/6B5A664BF0AF4F71B2F0BAA33141E2F1321242FBD5D19762F541EC971ACB0865",  # noqa: E501
+            "expected": {
+                "contract": "0x0000000000000000000000000000000000000000",
+                "auto_contract": "",
+            },
+        },
+    ]:
+        denom = case["denom"]
+        expected = case["expected"]
+        assert cli.query_contract_by_denom(denom) == expected
+        url = f"http://127.0.0.1:{port}/cronos/v1/contract_by_denom?denom={denom}"
+        assert requests.get(url).json() == expected
