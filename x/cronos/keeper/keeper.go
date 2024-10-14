ea0868ba1f9bbde567040a4356c1ab5a43c40459
yihuang <huang@crypto.com>
2024-10-14 15:46:25 +0800

    Update x/cronos/keeper/keeper.go
    Co-authored-by: mmsqe <mavis@crypto.com>
    Signed-off-by: yihuang <huang@crypto.com>

--- x/cronos/keeper/keeper.go
+++ x/cronos/keeper/keeper.go
@@ -317,7 +317,7 @@ func (k Keeper) IBCOnAcknowledgementPacketCallback(
 	if err := k.cdc.UnmarshalJSON(acknowledgement, &ack); err != nil {
 		return err
 	}
-	if !ack.UnderlyingAppSuccess {
+	if !ack.Success() {
 		return k.onPacketResult(ctx, packet, false, relayer, contractAddress, packetSenderAddress)
 	}
 	var res channeltypes.Acknowledgement
