f3fac7106a31a66b05b610415544c93b7fed4830
HuangYi <huang@crypto.com>
2024-11-29 15:39:56 +0800

    Problem: integration test shell includes dev tools
    Solution:
    - remove them from integration test shell
      - some dev tools are moved into flake.nix
      - dapp is in contracts/shell.nix
      - solc is in nix/gen-binding-shell.nix

--- integration_tests/shell.nix
+++ integration_tests/shell.nix
@@ -2,27 +2,14 @@
   system ? builtins.currentSystem,
   pkgs ? import ../nix { inherit system; },
 }:
-let
-  renameExe = pkgs.callPackage ../nix/rename-exe.nix { };
-in
 pkgs.mkShell {
   buildInputs = [
-    pkgs.jq
-    pkgs.go
-    pkgs.gomod2nix
     (pkgs.callPackage ../. { coverage = true; }) # cronosd
     pkgs.start-scripts
     pkgs.go-ethereum
     pkgs.cosmovisor
-    pkgs.poetry
     pkgs.nodejs
-    pkgs.git
-    pkgs.dapp
-    (renameExe pkgs.solc-static-versions.solc_0_6_8 "solc-0.6.8" "solc06")
-    (renameExe pkgs.solc-static-versions.solc_0_8_21 "solc-0.8.21" "solc08")
     pkgs.test-env
-    pkgs.nixfmt-rfc-style
-    pkgs.rocksdb
     pkgs.chain-maind
     pkgs.hermes
     pkgs.rly
