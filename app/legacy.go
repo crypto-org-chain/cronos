0f3c773c4926743091e9844ea0e683e98ebe56ec
HuangYi <huang@crypto.com>
2024-12-05 17:41:37 +0800

    add authz

--- app/legacy.go
+++ app/legacy.go
@@ -3,13 +3,16 @@ package app
 import (
 	"github.com/cosmos/cosmos-sdk/codec"
 	cdctypes "github.com/cosmos/cosmos-sdk/codec/types"
+	"github.com/cosmos/cosmos-sdk/x/authz"
 	icaauthtypes "github.com/crypto-org-chain/cronos/v2/app/legacy/icaauth/types"
 )
 
 func RegisterLegacyCodec(cdc *codec.LegacyAmino) {
 	icaauthtypes.RegisterCodec(cdc)
+	authz.RegisterLegacyAminoCodec(cdc)
 }
 
 func RegisterLegacyInterfaces(registry cdctypes.InterfaceRegistry) {
 	icaauthtypes.RegisterInterfaces(registry)
+	authz.RegisterInterfaces(registry)
 }
