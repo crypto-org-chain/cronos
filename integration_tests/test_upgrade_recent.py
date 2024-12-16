ea4fd4b239b2894a6c90833fe509238d75668cd8
mmsqe <mavis@crypto.com>
2024-12-16 10:13:56 +0800

    add assert

--- integration_tests/test_upgrade_recent.py
+++ integration_tests/test_upgrade_recent.py
@@ -1,5 +1,6 @@
-import concurrent.futures
+import subprocess
 from concurrent.futures import ThreadPoolExecutor, as_completed
+from pathlib import Path
 
 import pytest
 import requests
@@ -38,32 +39,38 @@ def call_check(url, address, concurrent):
         ],
         "id": 1,
     }
-    params = []
-    for _ in range(batch):
-        params.append(param)
+    params = [param for _ in range(batch)]
     with ThreadPoolExecutor(concurrent) as executor:
         tasks = [executor.submit(call, url, params) for _ in range(0, concurrent)]
         results = [future.result() for future in as_completed(tasks)]
     assert len(results) == concurrent
 
 
-def call_trace(url):
-    res = requests.get(f"{url}/debug/pprof/trace?seconds=3")
-    if res.status_code == 200:
-        with open("trace.out", "wb") as file:
-            file.write(res.content)
-        print("saved trace.out")
-    else:
-        print(f"failed to retrieve data: {res.status_code}")
+def call_trace(url, tmp_path_factory):
+    res = requests.get(f"{url}/debug/pprof/trace?seconds=0.2")
+    assert res.status_code == 200, res
+    folder = tmp_path_factory.mktemp("trace")
+    trace = Path(folder / "trace.out")
+    syscall = Path(folder / "syscall.out")
+    with open(trace, "wb") as file:
+        file.write(res.content)
+    cmd = f"go tool trace -pprof=syscall {trace} > {syscall}"
+    subprocess.run(cmd, shell=True, check=True)
+    cmd = f"go tool pprof -top {syscall} | head -n 10"
+    res = subprocess.run(
+        cmd, shell=True, check=True, capture_output=True, text=True
+    ).stdout
+    print(res)
+    assert "rocksdb" in res and "create_iterator" not in res, res
 
 
-def test_cosmovisor_upgrade(custom_cronos):
+def test_cosmovisor_upgrade(custom_cronos, tmp_path_factory):
     c = custom_cronos
     do_upgrade(c, "v1.4", c.cosmos_cli().block_height() + 15)
     res = deploy_contract(c.w3, CONTRACTS["CheckpointOracle"])
     with ThreadPoolExecutor() as exec:
-        futures = [
-            exec.submit(call_trace, c.pprof_endpoint()),
+        tasks = [
+            exec.submit(call_trace, c.pprof_endpoint(), tmp_path_factory),
             exec.submit(call_check, c.w3_http_endpoint(), res.address, 100),
         ]
-        concurrent.futures.wait(futures)
+        [future.result() for future in as_completed(tasks)]
