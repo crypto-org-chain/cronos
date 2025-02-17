8c4c9f8937e4eaeee92d5c2ad7cc59153551559c
mmsqe <mavis@crypto.com>
2025-02-17 20:30:27 +0800

    run multi times

--- integration_tests/test_upgrade_recent.py
+++ integration_tests/test_upgrade_recent.py
@@ -11,6 +11,7 @@ from .utils import (
     KEYS,
     deploy_contract,
     do_upgrade,
+    send_transaction,
     sign_transaction,
     wait_for_new_blocks,
     wait_for_port,
@@ -29,22 +30,34 @@ def custom_cronos(tmp_path_factory):
 
 def test_cosmovisor_upgrade(custom_cronos):
     c = custom_cronos
-    do_upgrade(c, "v1.2", c.cosmos_cli().block_height() + 15)
+    cli = do_upgrade(c, "v1.2", c.cosmos_cli().block_height() + 15)
     wait_for_port(ports.evmrpc_port(c.base_port(1)))
     w3 = c.node_w3(1)
-    gas_price = w3.eth.gas_price
+    # gas_price = w3.eth.gas_price
     erc20 = deploy_contract(
         w3,
         CONTRACTS["TestERC20A"],
         key=KEYS["validator"],
-        gas_price=gas_price,
+        # gas_price=gas_price,
     )
+    tip_price = 1
+    amount = 10000
+    max_price = 10000000000000 + tip_price
+    tx = {
+        "to": "0x0000000000000000000000000000000000000000",
+        "value": amount,
+        "gas": 21000,
+        "maxFeePerGas": max_price,
+        "maxPriorityFeePerGas": tip_price,
+    }
+    txreceipt = send_transaction(w3, tx, KEYS["community"])
+    assert txreceipt.status == 1
 
     def transfer():
         tx = erc20.functions.transfer(ADDRS["community"], 10).build_transaction(
             {
                 "from": ADDRS["validator"],
-                "gasPrice": gas_price,
+                # "gasPrice": gas_price,
             }
         )
         signed = sign_transaction(w3, tx, KEYS["validator"])
@@ -78,7 +91,9 @@ def test_cosmovisor_upgrade(custom_cronos):
 
     c.supervisorctl("stop", "cronos_777-1-node0")
     time.sleep(3)
+    print(cli.changeset_fixdata(f"{c.base_dir}/node0/data/versiondb", dry_run=True))
     cli.changeset_fixdata(f"{c.base_dir}/node0/data/versiondb")
+    print(cli.changeset_fixdata(f"{c.base_dir}/node0/data/versiondb", dry_run=True))
     c.supervisorctl("start", "cronos_777-1-node0")
     wait_for_port(ports.evmrpc_port(c.base_port(0)))
     b0 = trace_blk(0)
