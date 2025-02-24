e1d336f24a4658297286360b58781855560ddf37
mmsqe <mavis@crypto.com>
2025-02-24 11:10:43 +0800

    Problem: conflict of index uniqueness constrain violation on trace api (#1753)
    * Problem: conflict of index uniqueness constrain violation on trace api
    
    * test
    
    * update deps
    
    * bump go

--- integration_tests/cosmoscli.py
+++ integration_tests/cosmoscli.py
@@ -1851,6 +1851,11 @@ class CosmosCLI:
             "changeset", "restore-versiondb", height, format, home=self.data_dir
         )
 
+    def changeset_fixdata(self, versiondb_dir, dry_run=False):
+        return self.raw(
+            "changeset", "fixdata", versiondb_dir, "--dry-run" if dry_run else None
+        )
+
     def dump_snapshot(self, height, tarball, format=3):
         return self.raw(
             "snapshots", "dump", height, format, home=self.data_dir, output=tarball
