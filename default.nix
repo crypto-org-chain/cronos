93a7613507c64cb1cdb56ee58536a897241bf9bb
Thomas Nguy <thomas.nguy@crypto.com>
2025-04-24 16:13:19 +0900

    update default.nix

--- default.nix
+++ default.nix
@@ -11,7 +11,7 @@
   nativeByteOrder ? true, # nativeByteOrder mode will panic on big endian machines
 }:
 let
-  version = "v1.4.0";
+  version = "v1.4.7";
   pname = "cronosd";
   tags = [
     "ledger"
