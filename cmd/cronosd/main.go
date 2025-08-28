655067972ae8f7856ac1ed2c5f7140c86b3adfe9
Randy Ang <randy.ang@crypto.com>
2025-08-28 15:06:37 +0800

    add pprof

--- cmd/cronosd/main.go
+++ cmd/cronosd/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	_ "net/http/pprof" 
 	"os"
+    "net/http"
+	"fmt"
 
 	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
 	"github.com/crypto-org-chain/cronos/v2/app"
@@ -9,6 +12,7 @@ import (
 )
 
 func main() {
+	fmt.Println(http.ListenAndServe("localhost:6060", nil))
 	rootCmd := cmd.NewRootCmd()
 	if err := svrcmd.Execute(rootCmd, cmd.EnvPrefix, app.DefaultNodeHome); err != nil {
 		os.Exit(1)
