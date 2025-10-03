66c7add8293c18d43369fb25339752a04b8ef066
Thomas Nguy <thomas.nguy@crypto.com>
2025-10-04 01:08:00 +0900

    add test

--- integration_tests/test_eip2935.py
+++ integration_tests/test_eip2935.py
@@ -2,7 +2,12 @@ from hexbytes import HexBytes
 
 from .cosmoscli import module_address
 from .network import Cronos
-from .utils import submit_gov_proposal
+from .utils import (
+    CONTRACTS,
+    deploy_contract,
+    submit_gov_proposal,
+    w3_wait_for_block,
+)
 
 
 def test_eip2935(cronos: Cronos):
@@ -18,7 +23,7 @@ def test_eip2935(cronos: Cronos):
         "0x3373fffffffffffffffffffffffffffffffffffffffe14604657602036036042575f3560014303811160"
         "4257611fff81430311604257611fff9006545f5260205ff35b5f5ffd5b5f35611fff60014303065500"
     )
-    create2_preinstall = {
+    history_storage_preinstall = {
         "name": "HistoryStorage",
         "address": history_storage_address,
         "code": expected_history_storage_code,
@@ -33,10 +38,23 @@ def test_eip2935(cronos: Cronos):
             {
                 "@type": msg,
                 "authority": authority,
-                "preinstalls": [create2_preinstall],
+                "preinstalls": [history_storage_preinstall],
             }
         ],
     )
 
     history_storage_code = w3.eth.get_code(history_storage_address)
     assert history_storage_code == HexBytes(expected_history_storage_code)
+
+    contract = deploy_contract(
+        w3,
+        CONTRACTS["TestEip2935"],
+    )
+
+    w3_wait_for_block(w3, w3.eth.block_number + 10, timeout=30)
+    start = w3.eth.block_number
+    w3_wait_for_block(w3, start + 10, timeout=30)
+    for i in range(0, 9):
+        block = w3.eth.get_block(start + 0)
+        stored = contract.caller.blockhashOpcode(start + 0)
+        assert stored == block.hash
