5ccd423a14f100f4e485d0fb6aa8fa4b96d11b60
Thomas <81727899+thomas-nguy@users.noreply.github.com>
2025-11-05 22:37:44 +0900

    release: v1.5.4 (#1902)

--- default.nix
+++ default.nix
@@ -11,7 +11,7 @@
   nativeByteOrder ? true, # nativeByteOrder mode will panic on big endian machines
 }:
 let
-  version = "v1.5.3";
+  version = "v1.5.4";
   pname = "cronosd";
   tags = [
     "ledger"
