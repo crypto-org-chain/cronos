3e5df9a609f5843a2326fc05b6c62062d2bfe4f4
songgaoye <songgao.ye@crypto.com>
2025-12-11 00:01:10 +0800

    update build_overlay.nix

--- nix/build_overlay.nix
+++ nix/build_overlay.nix
@@ -9,13 +9,6 @@ let
     if len == 0 then [ ] else final.lib.lists.take (len - 1) l ++ [ newVal ];
 in
 {
-  go_1_25 = super.go_1_25.overrideAttrs (old: rec {
-    version = "1.25.0";
-    src = final.fetchurl {
-      url = "https://go.dev/dl/go${version}.src.tar.gz";
-      hash = "sha256-S9AekSlyB7+kUOpA1NWpOxtTGl5DhHOyoG4Y4HciciU=";
-    };
-  });
   rocksdb = final.callPackage ./rocksdb.nix { };
   golangci-lint = final.callPackage ./golangci-lint.nix { };
 
