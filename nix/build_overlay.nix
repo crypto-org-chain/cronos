a98142d7e0f81386632defa064deec4c2a9c1b8a
songgaoye <songgao.ye@crypto.com>
2025-12-11 13:02:14 +0800

    update nix

--- nix/build_overlay.nix
+++ nix/build_overlay.nix
@@ -9,15 +9,6 @@ let
     if len == 0 then [ ] else final.lib.lists.take (len - 1) l ++ [ newVal ];
 in
 {
-  # Override go_1_24 to create go_1_25, inheriting Windows platform support
-  # See: https://github.com/crypto-org-chain/chain-main/pull/1220
-  go_1_25 = super.go_1_24.overrideAttrs (old: rec {
-    version = "1.25.0";
-    src = final.fetchurl {
-      url = "https://go.dev/dl/go${version}.src.tar.gz";
-      hash = "sha256-S9AekSlyB7+kUOpA1NWpOxtTGl5DhHOyoG4Y4HciciU=";
-    };
-  });
   rocksdb = final.callPackage ./rocksdb.nix { };
   golangci-lint = final.callPackage ./golangci-lint.nix { };
 
