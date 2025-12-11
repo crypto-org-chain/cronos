dfb1fd09d782e474526ba26ceef376f0cdbf5ce2
songgaoye <songgao.ye@crypto.com>
2025-12-11 14:39:26 +0800

    update build_overlay

--- nix/build_overlay.nix
+++ nix/build_overlay.nix
@@ -9,6 +9,32 @@ let
     if len == 0 then [ ] else final.lib.lists.take (len - 1) l ++ [ newVal ];
 in
 {
+  # Override go_1_24 to create go_1_25 with Windows platform support
+  # Native nixpkgs go_1_25 doesn't support Windows (x86_64-windows not in meta.platforms)
+  # By overriding go_1_24, we inherit its Windows support
+  # See: https://github.com/crypto-org-chain/chain-main/pull/1220
+  go_1_25 = super.go_1_24.overrideAttrs (old: rec {
+    version = "1.25.0";
+    src = final.fetchurl {
+      url = "https://go.dev/dl/go${version}.src.tar.gz";
+      hash = "sha256-S9AekSlyB7+kUOpA1NWpOxtTGl5DhHOyoG4Y4HciciU=";
+    };
+    # Filter out patches that don't apply to Go 1.25
+    patches = builtins.filter (
+      patch:
+      let
+        patchName = builtins.baseNameOf (builtins.toString patch);
+      in
+      !final.lib.hasSuffix "-iana-etc-1.17.patch" patchName
+    ) (old.patches or [ ]);
+    # Apply the iana-etc substitutions manually for Go 1.25
+    postPatch = (old.postPatch or "") + ''
+      substituteInPlace src/net/lookup_unix.go \
+        --replace 'open("/etc/protocols")' 'open("${final.iana-etc}/etc/protocols")'
+      substituteInPlace src/net/port_unix.go \
+        --replace 'open("/etc/services")' 'open("${final.iana-etc}/etc/services")'
+    '';
+  });
   rocksdb = final.callPackage ./rocksdb.nix { };
   golangci-lint = final.callPackage ./golangci-lint.nix { };
 
