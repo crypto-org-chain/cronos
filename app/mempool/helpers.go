f88d02591cb8a5c2e02e83098c6510dd89af1f9f
jay.tseng <jay.tseng@crypto.com>
2026-06-15 10:57:19 -0400

    feat(mempool): time PoolSnapshot lock-hold via telemetry

--- app/mempool/helpers.go
+++ app/mempool/helpers.go
@@ -4,12 +4,14 @@ import (
 	"context"
 	"math/bits"
 
+	"github.com/cosmos/cosmos-sdk/telemetry"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkmempool "github.com/cosmos/cosmos-sdk/types/mempool"
 )
 
 // PoolSnapshot returns a snapshot of the current mempool transactions.
 func PoolSnapshot(ctx context.Context, mp sdkmempool.Mempool) []sdk.Tx {
+	defer telemetry.MeasureSince(telemetry.Now(), "cronos", "mempool", "recheck", "snapshot")
 	snap := make([]sdk.Tx, 0, mp.CountTx())
 	sdkmempool.SelectBy(ctx, mp, nil, func(tx sdk.Tx) bool {
 		snap = append(snap, tx)
